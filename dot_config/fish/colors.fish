9ef0560a78b38b87fa00de4152b86874ae6c16ce
Peter Cardenas <petercardenas441@gmail.com>
2022-06-17 02:21:18 -0500

    Add dot_config/fish/colors.fish

---
+++ dot_config/fish/colors.fish
@@ -0,0 +1,25 @@
+set -U fish_color_normal normal
+set -U fish_color_command brgreen
+set -U fish_color_quote bryellow
+set -U fish_color_redirection brcyan
+set -U fish_color_end magenta
+set -U fish_color_error brred
+set -U fish_color_param brcyan
+set -U fish_color_selection white --bold --background=brblack
+set -U fish_color_search_match bryellow --background=brblack
+set -U fish_color_history_current --bold
+set -U fish_color_operator blue
+set -U fish_color_escape brcyan
+set -U fish_color_cwd green
+set -U fish_color_cwd_root red
+set -U fish_color_valid_path --underline
+set -U fish_color_autosuggestion brblack
+set -U fish_color_user brgreen
+set -U fish_color_host normal
+set -U fish_color_cancel -r
+set -U fish_pager_color_completion normal
+set -U fish_pager_color_description yellow bryellow
+set -U fish_pager_color_prefix white --bold --underline
+set -U fish_pager_color_progress brwhite --background=cyan
+set -U fish_color_match blue
+set -U fish_color_comment bryellow
