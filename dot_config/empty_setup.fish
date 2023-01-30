d8dcdb8fb1499866003c65c0e87bff7062da0e0c
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-01-29 16:00:42 -0800

    Update .config/setup.fish

--- dot_config/empty_setup.fish
+++ dot_config/empty_setup.fish
@@ -1,5 +1,14 @@
+# Install starship
+curl -sS https://starship.rs/install.sh | sh
+# Install nvim on apt or MacPorts
+
+########################
+##     VIM SETUP      ##
+########################
 # Install vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+# Install plugs
+vim -c ":PlugInstall"
 
 # Setup YCM for vim completion
 sudo apt install -y python3-dev
