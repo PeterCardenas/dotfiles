f54983f83cf133be2187a4a5aee1da2fc649791b
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-02-10 10:03:31 -0800

    Update .config/fish/interactive_config.fish

--- dot_config/fish/interactive_config.fish
+++ dot_config/fish/interactive_config.fish
@@ -71,6 +71,7 @@ abbr -a vim "nvim"
 
 abbr -a sofi "source $HOME/.config/fish/config.fish"
 abbr -a cheznous "chezmoi git pull -- --rebase; and chezmoi merge-all"
+abbr -a chezvous "chezmoi git pull -- --rebase; and chezmoi --interactive apply"
 abbr -a ce "chezmoi edit"
 
 # VSCode shell integration
