4e13ec7b562273db65412237cc5c40d24f3b61b8
Peter Cardenas <petercardenas@Peters-Air.attlocal.net>
2023-02-10 08:50:02 -0800

    Update .config/fish/interactive_config.fish

--- dot_config/fish/interactive_config.fish
+++ dot_config/fish/interactive_config.fish
@@ -70,7 +70,7 @@ abbr -a p "starship prompt"
 abbr -a vim "nvim"
 
 abbr -a sofi "source $HOME/.config/fish/config.fish"
-abbr -a cheznous "chezmoi git pull -- --rebase && chezmoi --interactive merge-all"
+abbr -a cheznous "chezmoi git pull -- --rebase; and chezmoi --interactive apply"
 abbr -a ce "chezmoi edit"
 
 # VSCode shell integration
