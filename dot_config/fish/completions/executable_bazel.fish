abedf234ddc9512fd491c41bc0dfc78ab8b4e668
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-03-08 11:02:58 -0800

    Update .config/fish/completions/bazel.fish

--- dot_config/fish/completions/executable_bazel.fish
+++ dot_config/fish/completions/executable_bazel.fish
@@ -53,7 +53,7 @@ function __bazel_complete_targets --argument-names query
     for file_changed in $files_changed
         # Echo each file in files_changed as a bazel target
         set -l bazel_suffix (echo $file_changed | string replace -r '\.(cc|py)' '')
-        set -l bazel_target "//$(string replace -r "^(.*)/([^/]*_test)" "$1$2" $bazel_suffix)"
+        set -l bazel_target "//$(string replace -r "^(.*)/([^/]+_test)" "$1$2" $bazel_suffix)"
         echo $bazel_target
     end
 
