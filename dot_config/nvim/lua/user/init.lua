ef7e043a5cbe8751e3e15426d20ab11da44ac3cb
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-03-30 16:44:56 -0700

    Update .config/nvim/lua/user/init.lua

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -360,19 +360,7 @@ local config = {
             },
 
             -- AI Autocomplete
-            ["codota/tabnine-nvim"] = {
-                run = "./dl_binaries.sh",
-                config = function()
-                    require("tabnine").setup {
-                        disable_auto_comment = true,
-                        accept_keymap = "<Tab>",
-                        dismiss_keymap = "<C-]>",
-                        debounce_ms = 800,
-                        suggestion_color = { gui = "#808080", cterm = 244 },
-                        exclude_filetypes = { "TelescopePrompt" }
-                    }
-                end,
-            }
+            ["github/copilot.vim"] = {},
 
             -- We also support a key value style plugin definition similar to NvChad:
             -- ["ray-x/lsp_signature.nvim"] = {
