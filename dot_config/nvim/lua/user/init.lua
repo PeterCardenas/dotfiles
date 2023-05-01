fa31df7d956e0847d0023df6066ce5793a8822c3
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-04-30 22:16:19 -0700

    Update .config/nvim/lua/user/init.lua

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -370,6 +370,23 @@ local config = {
       ["kevinhwang91/nvim-ufo"] = {
         requires = {
           "kevinhwang91/promise-async",
+          {
+            "luukvbaal/statuscol.nvim",
+            config = function()
+              local builtin = require "statuscol.builtin"
+              require("statuscol").setup {
+                segments = {
+                  { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+                  { text = { "%s" }, click = "v:lua.ScSa" },
+                  {
+                    text = { builtin.lnumfunc, " " },
+                    condition = { true, builtin.not_empty },
+                    click = "v:lua.ScLa",
+                  },
+                },
+              }
+            end,
+          },
         },
         config = function()
           require("ufo").setup {
