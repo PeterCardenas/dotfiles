691d6655e329a683a74446cd660994533fa8e797
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-07-07 03:34:10 -0700

    Update .config/nvim/lua/user/init.lua

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -382,9 +382,7 @@ local config = {
       -- Get protobuf imports working in python
       ["numine777/py-bazel.nvim"] = {
         requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-        config = function()
-          require("py-bazel").setup { library_path_marker = "bazel-out/k8-fastbuild/bin/" }
-        end,
+        config = function() end,
       },
 
       -- Nice git diff view
@@ -728,6 +726,8 @@ local config = {
         update_on_nvim_resize = true,
       },
     }
+    vim.notify("py bazel setup")
+    require("py-bazel").setup { library_path_marker = "bazel-out/k8-fastbuild/bin/" }
     -- vim.api.nvim_set_keymap("i", "<C-CR>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
     -- Set up custom filetypes
