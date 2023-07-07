9b47cac66f38b1a7de23a4b583066eb50108d990
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-07-07 03:14:53 -0700

    Update .config/nvim/lua/user/init.lua

--- dot_config/nvim/lua/user/init.lua
+++ dot_config/nvim/lua/user/init.lua
@@ -382,7 +382,7 @@ local config = {
       -- Get protobuf imports working in python
       ["numine777/py-bazel.nvim"] = {
         requires = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
-        config = function() require("py-bazel").setup { library_path_marker = "bazel-bin/" } end,
+        config = function() require("py-bazel").setup { library_path_marker = "bazel-out/k8-fastbuild/bin/" } end,
       },
 
       -- Nice git diff view
