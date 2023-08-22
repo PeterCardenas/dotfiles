d09ad3c62f8f6975d63d3a506c20399058f058ec
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-08-22 12:54:06 -0700

    Update .config/fish/interactive_config.fish

--- dot_config/fish/interactive_config.fish
+++ dot_config/fish/interactive_config.fish
@@ -75,7 +75,7 @@ abbr -a p "starship prompt"
 # Don't accidentally run vim
 abbr -a vim "nvim"
 abbr -a vi "nvim"
-abbr -s v "nvim"
+abbr -a v "nvim"
 
 abbr -a t "tmux detach-client; tmux a; or tmux"
 
