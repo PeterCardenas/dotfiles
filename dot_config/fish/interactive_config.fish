ee52e36749b5b042da45374657420dbcf55a99fa
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-09-06 23:32:36 -0700

    Update .config/fish/interactive_config.fish

--- dot_config/fish/interactive_config.fish
+++ dot_config/fish/interactive_config.fish
@@ -81,6 +81,7 @@ abbr -a vi "nvim"
 abbr -a v "nvim"
 
 abbr -a t "tmux detach-client; tmux a; or tmux"
+abbr -a tat "tmux attach -t"
 
 abbr -a sofi "source $HOME/.config/fish/config.fish"
 abbr -a cheznous "chezmoi git pull -- --rebase; and chezmoi merge-all"
