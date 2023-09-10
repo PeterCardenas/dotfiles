872068f54e1d0f87917de3c69ff6fc0e4694fdd5
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-09-09 20:32:40 -0700

    Update .config/fish/functions/fix_display.fish

--- dot_config/fish/functions/fix_display.fish
+++ dot_config/fish/functions/fix_display.fish
@@ -1,7 +1,7 @@
 function fix_display
-  set -l tmux_display (tmux show-environment DISPLAY | cut -d= -f2)
   # If tmux is running, set DISPLAY to tmux's value.
-  if test -n "$tmux_display"
+  if test -n "$TMUX"
+    set -l tmux_display (tmux show-environment DISPLAY | cut -d= -f2)
     set -gx DISPLAY "$tmux_display"
   else
     # Otherwise, set DISPLAY to hostname's value.
