f42d5fe444315773b42042a6bc9d1e7437f20518
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-09-17 06:05:57 -0700

    Update .config/fish/completions/ssh-et.fish

--- dot_config/fish/completions/ssh-et.fish
+++ dot_config/fish/completions/ssh-et.fish
@@ -1,4 +1,4 @@
 # Copies the completions from ssh to ssh-et.
 # Not all commands are supported, but this is the lowest lift work.
 # Reference: https://github.com/infokiller/ssh-et
-complete -c ssh-et -a '(__fish_use_subcommand)' -n '__fish_seen_subcommand_from ssh'
+complete -c ssh-et -c ssh
