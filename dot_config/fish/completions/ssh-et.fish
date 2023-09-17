ce9c602d4af521055d2e520860ffa7a5c56aa409
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-09-17 06:06:45 -0700

    Remove .config/fish/completions/ssh-et.fish Update .config/fish/interactive_config.fish

--- dot_config/fish/completions/ssh-et.fish
+++
@@ -1,4 +0,0 @@
-# Copies the completions from ssh to ssh-et.
-# Not all commands are supported, but this is the lowest lift work.
-# Reference: https://github.com/infokiller/ssh-et
-complete -c ssh-et -c ssh
