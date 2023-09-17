578aa2169992a165a9799778e13d393ab2aa680d
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-09-17 06:27:32 -0700

    Update .config/fish/functions/refresh_tmux_vars.fish

--- dot_config/fish/functions/refresh_tmux_vars.fish
+++ dot_config/fish/functions/refresh_tmux_vars.fish
@@ -1,3 +1,4 @@
+# Add tmux variables to fish shell before a command is executed.
 function refresh_tmux_vars --on-event="fish_preexec"
   if set -q TMUX
     tmux showenv -s | string replace -rf '^((?:SSH|DISPLAY).*?)=(".*?"); export.*' 'set -gx $1 $2' | source
