5d3851ffa7bc2941557f423ad4c25d524d68183a
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-09-17 13:57:03 -0700

    Update .config/fish/functions/fix_display.fish Update .config/fish/functions/refresh_tmux_vars.fish

--- dot_config/fish/functions/fix_display.fish
+++ dot_config/fish/functions/fix_display.fish
@@ -1,6 +1,6 @@
 function fix_display
   # If tmux is running, set DISPLAY to tmux's value.
-  if test -n "$TMUX"
+  if set -q TMUX
     set -l tmux_display (tmux show-environment DISPLAY | cut -d= -f2)
     set -gx DISPLAY "$tmux_display"
   else if string match -q -r "^:[0-9]\$" $DISPLAY
--- dot_config/fish/functions/refresh_tmux_vars.fish
+++ dot_config/fish/functions/refresh_tmux_vars.fish
@@ -1,6 +1,6 @@
 # Add tmux variables to fish shell before a command is executed.
-function refresh_tmux_vars --on-event="fish_preexec"
+function refresh_tmux_vars --on-event fish_preexec
   if set -q TMUX
-    tmux showenv -s | string replace -rf '^((?:SSH|DISPLAY).*?)=(".*?"); export.*' 'set -gx $1 $2' | source
+    tmux showenv | string replace -rf '^((?:SSH|DISPLAY|XAUTHORITY).*?)=(.*?)$' 'set -gx $1 "$2"' | source
   end
 end
