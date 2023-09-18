5d958c8c8c815d4235fbcfb610c13bb6ec961bed
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-09-17 19:53:57 -0700

    Update .config/fish/completion_utils.fish

--- dot_config/fish/completion_utils.fish
+++ dot_config/fish/completion_utils.fish
@@ -2,7 +2,7 @@ function __fish_is_token_eq_nth
   set tokens (commandline -poc)
   set token_str (string replace -ra '(^|\-).+?\s+' '' -- $tokens)
   set token_str (string replace -ra '\s+' ' ' -- $token_str)
-  set tokens (string split " " $token_str)
+  set tokens (string split " " "$token_str")
   if [ (count $tokens) -eq $argv[1] ]
     return 0
   end
