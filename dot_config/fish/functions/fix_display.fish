497f2ba056c12865eea987103e3b3e040151aae7
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-09-17 20:51:07 -0700

    Update .config/fish/functions/fix_display.fish

--- dot_config/fish/functions/fix_display.fish
+++ dot_config/fish/functions/fix_display.fish
@@ -3,7 +3,9 @@ function fix_display
   if set -q TMUX
     set -l tmux_display (tmux show-environment DISPLAY | cut -d= -f2)
     set -gx DISPLAY "$tmux_display"
-  else if string match -q -r "^:[0-9]\$" $DISPLAY
+  else if set -q DISPLAY; and string match -q -r "^:[0-9]\$" $DISPLAY
     set -gx DISPLAY "$(hostname)$DISPLAY"
+  else if not set -q DISPLAY
+    set -gx DISPLAY "$(hostname):0"
   end
 end
