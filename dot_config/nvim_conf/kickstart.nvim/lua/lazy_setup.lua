364ff63877eba4452fc7dfb2714a2003f3826601
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-10-08 05:10:10 -0700

    :sparkles: add better ui for code actions and renames

--- dot_config/nvim_conf/kickstart.nvim/lua/lazy_setup.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/lazy_setup.lua
@@ -67,12 +67,34 @@ require('lazy').setup({
     end
   },
 
-  -- Better notification UI
+  -- Better UI for select, notifications, popups, and many others.
   {
-    'rcarriga/nvim-notify',
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "rcarriga/nvim-notify",
+    },
     config = function()
-      vim.notify = require('notify')
-    end,
+      require("noice").setup({
+        lsp = {
+          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        -- you can enable a preset for easier configuration
+        presets = {
+          bottom_search = true,   -- use a classic bottom cmdline for search
+          command_palette = true, -- position the cmdline and popupmenu together
+          long_message_to_split = true, -- long messages will be sent to a split
+          inc_rename = false,     -- enables an input dialog for inc-rename.nvim
+          lsp_doc_border = false, -- add a border to hover docs and signature help
+        },
+      })
+    end
   },
 
   {
@@ -116,7 +138,7 @@ require('lazy').setup({
   { 'numToStr/Comment.nvim', opts = {} },
 
   -- Camel-case and snake-case motion
-  { "bkad/CamelCaseMotion", event = "BufEnter" },
+  { "bkad/CamelCaseMotion",  event = "BufEnter" },
 
   -- Sticky scroll
   {
