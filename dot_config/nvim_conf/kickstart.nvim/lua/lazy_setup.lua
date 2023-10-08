12a054ed0a61f1c13c372269050c2398ccf91983
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-10-08 00:57:09 -0700

    :wrench: [neovim] support indent-blankline v3

--- dot_config/nvim_conf/kickstart.nvim/lua/lazy_setup.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/lazy_setup.lua
@@ -94,12 +94,14 @@ require('lazy').setup({
   {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    main = 'ibl',
+    config = function()
+      require('ibl').setup({
+        indent = {
+          char = '┊',
+        },
+      })
+    end,
   },
 
   -- "gc" to comment visual regions/lines
