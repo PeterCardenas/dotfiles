e81bc3c33717973958170a27900f722557ac0c0d
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-10-27 09:47:03 -0700

    :soap: [neovim] don't underline for scope

--- dot_config/nvim_conf/kickstart.nvim/lua/lazy_setup.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/lazy_setup.lua
@@ -157,6 +157,10 @@ require('lazy').setup({
     main = 'ibl',
     config = function()
       require('ibl').setup({
+        scope = {
+          show_start = false,
+          show_end = false,
+        },
         indent = {
           char = '┊',
         },
