d71f4eeb2fd060dd9529497006ffc1cd5cd745ec
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-10-29 12:43:52 -0700

    :wrench: [neovim/tmux] allow pane navigation when zoomed neovim

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/tmux.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/tmux.lua
@@ -55,7 +55,6 @@ return {
   'alexghergh/nvim-tmux-navigation',
   config = function()
     require 'nvim-tmux-navigation'.setup {
-      disable_when_zoomed = true, -- defaults to false
       keybindings = {
         left = "<C-h>",
         down = "<C-j>",
