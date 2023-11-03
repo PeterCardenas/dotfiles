0c649032d54f09445bd340796affc8935de1b37f
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-11-02 18:33:11 -0700

    :sparkles: sort session by most recently used and remove current session from list

--- dot_config/fish/functions/manage_sessions.fish
+++ dot_config/fish/functions/manage_sessions.fish
@@ -1,5 +1,10 @@
 function manage_sessions -d "Navigate to an existing session"
-  set session_names (tmux list-sessions -F "#{session_name}")
+  # Sort sessions by last activity, with most recent first.
+  set session_activities (tmux list-sessions -F "#{session_name}|#{session_activity}" | sort -t "|" -k2 -r)
+  # Exclude the current session.
+  set session_activities $session_activities[2..-1]
+  # Extract the session names.
+  set session_names (printf "%s\n" $session_activities | cut -d "|" -f 1)
   set selected_session (printf "%s\n" $session_names | fzf --prompt "Session: " --preview "fish -c 'preview_session {}'")
   if test -n "$selected_session"
     tmux switch-client -t $selected_session
