d37c6a24d5549354019447f79b7d804d69f80395
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-11-02 23:56:25 -0700

    :sparkles: [chezmoi/neovim] automatically keep lazy lock in sync

--- dot_config/nvim_conf/kickstart.nvim/lua/local/chezmoi.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/local/chezmoi.lua
@@ -1,3 +1,5 @@
+local chezmoi_augroup = vim.api.nvim_create_augroup("Chezmoi", { clear = true })
+
 vim.api.nvim_create_autocmd("BufWritePost", {
   callback = function(args)
     local bufnr = args.buf
@@ -7,6 +9,28 @@ vim.api.nvim_create_autocmd("BufWritePost", {
     end
     vim.system({ "chezmoi", "apply", "--source-path", filepath })
   end,
-  group = vim.api.nvim_create_augroup("ChezmoiApply", { clear = true }),
+  group = chezmoi_augroup,
   pattern = "*",
+})
+
+local function track_lazy_lock()
+  local symlinked_lazy_lock_file_path = os.getenv("HOME") .. "/.config/nvim/lazy-lock.json"
+  local lazy_lock_file_path_unformatted = vim.system({ "realpath", symlinked_lazy_lock_file_path }):wait()
+      .stdout
+  if not lazy_lock_file_path_unformatted then
+    vim.notify("lazy-lock.json not found" .. symlinked_lazy_lock_file_path, vim.log.levels.ERROR)
+    return
+  end
+  local lazy_lock_file_path = lazy_lock_file_path_unformatted:gsub("\n", "")
+  local chezmoi_add_metadata = vim.system({ "chezmoi", "add", lazy_lock_file_path }):wait()
+  if chezmoi_add_metadata.code ~= 0  then
+    vim.notify("chezmoi add failed: " .. chezmoi_add_metadata.stderr, vim.log.levels.ERROR)
+    return
+  end
+end
+
+vim.api.nvim_create_autocmd("User", {
+  callback = track_lazy_lock,
+  group = chezmoi_augroup,
+  pattern = { "LazyInstall", "LazyUpdate", "LazyClean" }
 })
