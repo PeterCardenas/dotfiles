f1fab5f793992b2d7b249ec7f5ec6125d5c12b9b
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2023-12-26 00:11:24 -0800

    :soap: [neovim] remove astronvim
    don't have any inspiration from astronvim anymore, removing is good for productivity

--- dot_config/nvim_conf/astronvim/lua/configs/notify.lua
+++
@@ -1,9 +0,0 @@
-local notify = require "notify"
-notify.setup(astronvim.user_plugin_opts("plugins.notify", { stages = "fade" }))
-vim.notify = function(msg, ...)
-    if msg:match("warning: multiple different client offset_encodings") then
-        return
-    end
-
-    notify(msg, ...)
-end
