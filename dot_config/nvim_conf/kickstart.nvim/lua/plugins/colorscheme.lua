e35954ed2f31e7007e55227335f82d8486ea5045
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-01-02 01:17:53 -0800

    :wrench: [neovim] make hard-to-read-highlights brighter
    closes #1

---
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,23 @@
+---@type LazyPluginSpec
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    vim.cmd('colorscheme tokyonight-storm')
+    -- Make comments foreground brighter.
+    vim.cmd [[highlight Comment cterm=italic gui=italic guifg=#7c7c7c]]
+    -- More contrast for selected tab.
+    vim.cmd [[highlight TabLineSel guifg=#e0e0e0 guibg=#26355E]]
+    -- Make tab title foreground brighter.
+    vim.cmd [[highlight TabLine guifg=#7c7c7c guibg=#161F38]]
+    -- Make git signs foreground brighter.
+    vim.cmd [[highlight GitSignsAdd guifg=#258F4E]]
+    vim.cmd [[highlight GitSignsChange guifg=#DBC614]]
+    vim.cmd [[highlight GitSignsDelete guifg=#DB4539]]
+    -- Make leap backdrop foreground light grey (in case I forget the key to press lol).
+    vim.cmd [[highlight LeapBackdrop guifg=#5C5C5C]]
+    -- Add red close button highlight.
+    vim.cmd [[highlight TabLineClose guifg=#DB4539]]
+  end,
+}
