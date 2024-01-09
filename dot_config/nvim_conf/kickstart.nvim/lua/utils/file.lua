2f1f8be8fa7148c872fb0711819b8d7b89783c8a
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-01-08 17:16:33 -0800

    :wrench: create file utils

---
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/file.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+---@param name string
+---@return boolean
+function M.file_exists(name)
+  local f = io.open(name, "r")
+  if f ~= nil then
+    io.close(f)
+    return true
+  end
+  return false
+end
+
+---@return string
+function M.get_cwd()
+  return os.getenv("PWD") or io.popen("cd"):read()
+end
+
+return M
