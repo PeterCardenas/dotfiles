5d6d9a6cab7740d13ed63f22d8f9b0e41f914bb8
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-03-12 14:47:04 -0700

    :wrench: manually add dirs to path in fish config

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -2,6 +2,10 @@ if status is-interactive
   source $HOME/.config/fish/interactive_config.fish
 end
 
+set -gx PNPM_HOME "$HOME/.local/share/pnpm"
+set BOB_NVIM_BIN "$HOME/.local/share/bob/nvim-bin"
+set LOCAL_BIN "$HOME/.local/bin"
+fish_add_path -P $PNPM_HOME $BOB_NVIM_BIN $LOCAL_BIN
 
 if not set -q FAST_PROMPT
   # MUST BE AT END OF FILE
