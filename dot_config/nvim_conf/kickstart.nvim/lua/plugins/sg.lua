ab03f7b7380c1413fb5bee531cb6e8ab6118d3b9
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-04-10 08:17:16 -0700

    ✨ lazy load plugin on ex commands

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/sg.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/sg.lua
@@ -1,22 +1,8 @@
-vim.api.nvim_create_user_command('CodyInit',
-  function()
-    require('sg')
-  end,
-  { nargs = 0, desc = "Load the Cody Module" }
-)
-
-vim.api.nvim_create_user_command('CopilotChatInit',
-  function()
-    require('CopilotChat')
-  end,
-  { nargs = 0, desc = "Load the Copilot Chat Module" }
-)
-
 ---@type LazyPluginSpec[]
 return {
   {
     'sourcegraph/sg.nvim',
-    lazy = true,
+    cmd = { 'CodyDo', 'CodyChat' },
     dependencies = {
       'nvim-lua/plenary.nvim',
       'nvim-telescope/telescope.nvim',
@@ -28,7 +14,7 @@ return {
   {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
-    lazy = true,
+    cmd = { 'CopilotChatToggle' },
     dependencies = {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim" },
