032b81878fbfa04c689c26393ff1efbecae8dabd
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-05-05 13:44:20 -0700

    🔧 use jq instead of jsonls for formatting

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/lsp/on_attach.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/lsp/on_attach.lua
@@ -24,6 +24,11 @@ function M.on_attach(client, bufnr)
     client.server_capabilities.documentFormattingProvider = false
     client.server_capabilities.documentRangeFormattingProvider = false
   end
+  if client.name == 'jsonls' then
+    -- Defer to jq for formatting.
+    client.server_capabilities.documentFormattingProvider = false
+    client.server_capabilities.documentRangeFormattingProvider = false
+  end
   if client.name == 'gopls' then
     if not client.server_capabilities.semanticTokensProvider then
       local semantic = client.config.capabilities.textDocument.semanticTokens
