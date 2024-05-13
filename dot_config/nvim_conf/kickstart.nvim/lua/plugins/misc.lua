ab954041f1b443c9ad6d6f8b3755c32ff0af9fc6
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-05-13 04:20:38 -0700

    🧹 make neoscroll lazy loaded

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/misc.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/misc.lua
@@ -70,41 +70,6 @@ return {
     end,
   },
 
-  -- Smooth scrolling
-  {
-    'karb94/neoscroll.nvim',
-    keys = {
-      '<C-u>',
-      '<C-d>',
-      '<C-b>',
-      '<C-f>',
-      '<C-y>',
-      '<C-e>',
-      'zt',
-      'zz',
-      'zb',
-    },
-    config = function()
-      require('neoscroll').setup({
-        cursor_scrolls_alone = false,
-        stop_eof = false,
-      })
-      local t = {}
-      -- Syntax: t[keys] = {function, {function arguments}}
-      t['<C-u>'] = { 'scroll', { '-vim.wo.scroll', 'true', '150' } }
-      t['<C-d>'] = { 'scroll', { 'vim.wo.scroll', 'true', '150' } }
-      t['<C-b>'] = { 'scroll', { '-vim.api.nvim_win_get_height(0)', 'true', '75' } }
-      t['<C-f>'] = { 'scroll', { 'vim.api.nvim_win_get_height(0)', 'true', '75' } }
-      t['<C-y>'] = { 'scroll', { '-0.10', 'false', '75' } }
-      t['<C-e>'] = { 'scroll', { '0.10', 'false', '75' } }
-      t['zt'] = { 'zt', { '75' } }
-      t['zz'] = { 'zz', { '75' } }
-      t['zb'] = { 'zb', { '75' } }
-
-      require('neoscroll.config').set_mappings(t)
-    end,
-  },
-
   -- Delete buffers more reliably
   {
     'famiu/bufdelete.nvim',
