c8d6b4d908d71084aa1fa09c03e57b5ca14ba35d
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-05-26 17:52:21 -0700

    ✨ add simple desktop notifications

---
+++ dot_config/fish/functions/notify.fish
@@ -0,0 +1,26 @@
+function notify -d "Send notifications via terminal escape sequences" -a title -a message
+    set -l osc_prefix
+    if set -q TMUX
+        # Assume that tmux sessions in ssh sessions are nested.
+        if set -q SSH_CONNECTION
+            set osc_prefix "\x1bPtmux;\x1b\x1bPtmux;\x1b\x1b\x1b\x1b"
+        else
+            set osc_prefix "\x1bPtmux;\x1b\x1b"
+        end
+    else
+        set osc_prefix "\x1b"
+    end
+    set -l notify_prefix "$osc_prefix]777;notify;"
+    set -l osc_suffix
+    if set -q TMUX
+        if set -q SSH_CONNECTION
+            set osc_suffix "\a\x1b\x1b\\\\\x1b\\"
+        else
+            set osc_suffix "\a\x1b\\"
+        end
+    else
+        set osc_suffix "\x1b\\"
+    end
+    set -l sequence "$notify_prefix$title;$message$osc_suffix"
+    printf $sequence
+end
