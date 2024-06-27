4c160836e9475ec929c8fbcfa3681af323b31da5
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-06-27 11:00:47 -0700

    🔧 fix autoloading of typescript plugin

---
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/typescript.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+M.SUPPORTED_FT = {
+  'typescript',
+  'typescriptreact',
+  'typescript.tsx',
+  'typescript.jsx',
+  'javascript',
+  'javascriptreact',
+  'javascript.jsx',
+}
+
+return M
