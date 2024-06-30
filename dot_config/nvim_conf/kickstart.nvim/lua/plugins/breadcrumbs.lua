8cffa1592a9e6665672a168b76b6dae622b52490
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-06-30 12:11:12 -0700

    🔧 always show breadcrumbs for valid buffers
    previously we were only showing the breadcrumbs if the current filetype
    had a treesitter parser

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/breadcrumbs.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/breadcrumbs.lua
@@ -46,6 +46,11 @@ local DROPBAR_CONFIG = {
       end,
     },
   },
+  general = {
+    enable = function(buf, win)
+      return vim.fn.win_gettype(win) == '' and vim.wo[win].winbar == '' and vim.bo[buf].bt == '' and (buf and vim.api.nvim_buf_is_valid(buf) and true or false)
+    end,
+  },
 }
 
 vim.keymap.set({ 'n', 'v' }, '<C-o>', function()
