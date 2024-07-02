b6583990b70789f41075d446dc3d45ac02a8ec5b
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-07-01 18:31:55 -0700

    🔧 support sessions with spaces

--- dot_config/fish/functions/manage_sessions.fish
+++ dot_config/fish/functions/manage_sessions.fish
@@ -5,7 +5,7 @@ function manage_sessions -d "Navigate to an existing session"
     set session_activities $session_activities[2..-1]
     # Extract the session names.
     set session_names (printf "%s\n" $session_activities | cut -d "|" -f 1)
-    set selected_session (printf "%s\n" $session_names | fzf --reverse --prompt "Session: " --preview "fish -c 'preview_session {}'")
+    set selected_session (printf "%s\n" $session_names | fzf --reverse --prompt "Session: " --preview "fish -c \"preview_session {}\"")
     if test -n "$selected_session"
         tmux switch-client -t $selected_session
     end
