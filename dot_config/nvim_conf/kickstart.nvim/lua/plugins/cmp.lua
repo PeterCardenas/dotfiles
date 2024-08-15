294ef4d6d0062a8045fd14fb62e6b4cd5730d02b
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-08-14 18:55:39 -0700

    🔧 overwrite inlay hints toggle keymap

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/cmp.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/cmp.lua
@@ -34,12 +34,12 @@ return {
     local luasnip = require('luasnip')
     require('luasnip.loaders.from_vscode').lazy_load()
     luasnip.config.setup({})
-    vim.keymap.set({ 'i', 's' }, '<Tab>', function()
+    vim.keymap.set({ 'n', 'i', 's' }, '<Tab>', function()
       if luasnip.expand_or_jumpable() then
         return luasnip.expand_or_jump()
       end
     end)
-    vim.keymap.set({ 'i', 's' }, '<S-Tab>', function()
+    vim.keymap.set({ 'n', 'i', 's' }, '<S-Tab>', function()
       if luasnip.jumpable(-1) then
         return luasnip.jump(-1)
       end
