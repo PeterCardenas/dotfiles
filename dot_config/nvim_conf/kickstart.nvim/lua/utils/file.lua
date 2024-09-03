b3bb418c4f86ff37582e754b00c28b57e03e1fa8
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-09-02 20:04:23 -0700

    🧹 make bazel lint use async await

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/file.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/file.lua
@@ -16,28 +16,32 @@ function M.get_cwd()
   return os.getenv('PWD') or io.popen('cd'):read()
 end
 
----Gets the directory of the git root from the given file/directory.
----This avoids using other plugins for startup time.
----@param file_or_dir string? If not given, the current working directory is used.
----@return string|nil
-function M.get_git_root(file_or_dir)
-  local target_dir = vim.fn.getcwd()
-  if file_or_dir ~= nil then
-    if vim.fn.isdirectory(file_or_dir) == 1 then
-      target_dir = file_or_dir
+function M.get_ancestor_dir(target_filename, start_path)
+  local target_dir = M.get_cwd()
+  if start_path ~= nil then
+    if vim.fn.isdirectory(start_path) == 1 then
+      target_dir = start_path
     else
-      target_dir = vim.fn.fnamemodify(file_or_dir, ':h')
+      target_dir = vim.fn.fnamemodify(start_path, ':h')
     end
   end
-  local home_dir = vim.fn.expand('~')
+  local home_dir = os.getenv('HOME')
   while target_dir ~= '' and target_dir ~= home_dir do
-    local git_dir = target_dir .. '/.git'
-    if vim.fn.isdirectory(git_dir) == 1 or vim.fn.filereadable(git_dir) then
+    local ancestor_dir = target_dir .. '/' .. target_filename
+    if vim.fn.isdirectory(ancestor_dir) == 1 or vim.fn.filereadable(ancestor_dir) then
       return target_dir
     end
     target_dir = vim.fn.fnamemodify(target_dir, ':h')
   end
   return nil
+end
+
+---Gets the directory of the git root from the given file/directory.
+---This avoids using other plugins for startup time.
+---@param file_or_dir string? If not given, the current working directory is used.
+---@return string|nil
+function M.get_git_root(file_or_dir)
+  return M.get_ancestor_dir('.git', file_or_dir)
 end
 
 ---Checks whether the given file is in the given directory.
