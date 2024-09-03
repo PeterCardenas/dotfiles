f750c2128939dd6b3a4441724b6ad1b78cbbe643
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-09-02 23:32:47 -0700

    🔧 make luals somewhat bearable

---
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/lsp.lua
@@ -0,0 +1,26 @@
+local M = {}
+
+M.FT_WITH_LSP = {
+  'typescript',
+  'typescriptreact',
+  'javascript',
+  'javascriptreact',
+  'css',
+  'scss',
+  'cpp',
+  'python',
+  'go',
+  'json',
+  'lua',
+  'bzl',
+  'mdx',
+  'markdown',
+  'rust',
+  'yaml',
+  'sh',
+  'fish',
+  'toml',
+  'jsonc',
+}
+
+return M
