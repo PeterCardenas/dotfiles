e9f54e9ba541ca9c4cb64c5e270d900008494011
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-09-02 20:04:23 -0700

    🧹 make bazel lint use async await

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/lint_format.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/lint_format.lua
@@ -9,9 +9,10 @@ vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter' }, {
 local nogo_diagnostic_ns = vim.api.nvim_create_namespace('GoBazelLint')
 
 ---Map of filenames to functions that will lint the file.
----@type table<string, fun(): nil>
+---@type table<string, async fun(): nil>
 local bazel_go_lint_queue = {}
 
+---@async
 local function enqueue_next_bazel_go_lint()
   local filename, exec = next(bazel_go_lint_queue)
   if filename then
@@ -20,11 +21,10 @@ local function enqueue_next_bazel_go_lint()
   end
 end
 
+---@async
 ---@param abs_filepath string
 local function bazel_go_lint(abs_filepath)
-  ---@type fun(path: string): string|nil
-  local get_workspace_root = require('lspconfig.util').root_pattern('WORKSPACE')
-  local workspace_root = get_workspace_root(abs_filepath)
+  local workspace_root = require('utils.file').get_ancestor_dir('WORKSPACE')
   if not workspace_root then
     enqueue_next_bazel_go_lint()
     return
@@ -35,102 +35,80 @@ local function bazel_go_lint(abs_filepath)
   local current_filename = vim.fn.fnamemodify(abs_filepath, ':t')
   local relative_parent_dir = string.sub(relative_filepath, 1, string.len(relative_filepath) - string.len(current_filename) - 1)
   local query_targets = string.format('kind(go_*, rdeps(//%s/..., %s, 1))', relative_parent_dir, relative_filepath)
-  local stdout = vim.loop.new_pipe()
-  ---@type string[]
-  local matched_targets = {}
-  local handle = vim.loop.spawn('fish', {
-    args = { '-c', string.format('bazel %s query "%s"', output_base_flag, query_targets) },
-    cwd = workspace_root,
-    stdio = { nil, stdout, nil },
-  })
-  if not handle then
-    vim.notify('Failed to spawn bazel query', vim.log.levels.ERROR)
+  success, output = shell.async_cmd('fish', { '-c', string.format('bazel %s query --color=no "%s"', output_base_flag, query_targets) })
+  if not success then
+    vim.notify('Failed to bazel query for go', vim.log.levels.ERROR)
     enqueue_next_bazel_go_lint()
     return
   end
-  vim.loop.read_start(stdout, function(query_err, query_data)
-    assert(not query_err, query_err)
-    if query_data then
-      if string.match(query_data, '^//') then
-        table.insert(matched_targets, query_data)
-      end
-    else
-      stdout:close()
-      local stderr = vim.loop.new_pipe()
-      handle = vim.loop.spawn('fish', {
-        args = { '-c', string.format('bazel %s build %s', output_base_flag, table.concat(matched_targets, ' ')) },
-        cwd = workspace_root,
-        stdio = { nil, nil, stderr },
-      })
-      if not handle then
-        enqueue_next_bazel_go_lint()
-        vim.notify('Failed to spawn bazel build', vim.log.levels.ERROR)
-        return
-      end
-
-      ---@type table<string, lsp.Diagnostic[]>
-      local file_diagnostics = {}
-      vim.loop.read_start(stderr, function(err, data)
-        assert(not err, err)
-        if data then
-          ---@param line string
-          local function parse_line(line)
-            local filename, line_num_str, col_num_str, error_msg = string.match(line, '^(%S+):(%d+):(%d+): (.+)$')
-            if not filename then
-              return
-            end
-            local line_num, col_num = tonumber(line_num_str), tonumber(col_num_str)
-            if line_num == nil or col_num == nil then
-              return
-            end
-            ---@type vim.Diagnostic
-            local diagnostic = {
-              source = 'bazel-go-build',
-              message = error_msg,
-              range = {
-                start = { line = line_num - 1, character = col_num - 1 },
-                ['end'] = { line = line_num - 1, character = col_num - 1 },
-              },
-              severity = vim.diagnostic.severity.ERROR,
-              lnum = line_num - 1,
-              col = col_num - 1,
-            }
-            if not file_diagnostics[filename] then
-              file_diagnostics[filename] = {}
-            end
-            table.insert(file_diagnostics[filename], diagnostic)
-          end
-          local lines = vim.split(data, '\n')
-          for _, line in ipairs(lines) do
-            parse_line(line)
-          end
-        else
-          stderr:close()
-          local function filename_to_bufnr(filename)
-            local file_uri = vim.uri_from_fname(workspace_root .. '/' .. filename)
-            local bufnr = vim.uri_to_bufnr(file_uri)
-            if not vim.api.nvim_buf_is_loaded(bufnr) then
-              vim.fn.bufload(bufnr)
-            end
-            vim.bo[bufnr].buflisted = true
-            return bufnr
-          end
-
-          local function set_diagnostics()
-            vim.diagnostic.reset(nogo_diagnostic_ns)
-            for filename, diagnostics in pairs(file_diagnostics) do
-              local bufnr = filename_to_bufnr(filename)
-              vim.diagnostic.set(nogo_diagnostic_ns, bufnr, diagnostics, { underline = true })
-              enqueue_next_bazel_go_lint()
-            end
-          end
-          vim.schedule(set_diagnostics)
-        end
-      end)
-      handle:close()
+  ---@type string[]
+  local matched_targets = {}
+  for _, line in ipairs(output) do
+    -- TODO: Make regex more robust.
+    local target = string.match(line, '//[a-zA-Z_:/]+$')
+    if target then
+      table.insert(matched_targets, target)
     end
-  end)
-  vim.loop.close(handle)
+  end
+
+  success, output = shell.async_cmd('fish', { '-c', string.format('bazel %s build --color=no %s', output_base_flag, table.concat(matched_targets, ' ')) })
+  ---@type table<string, lsp.Diagnostic[]>
+  local file_diagnostics = {}
+  ---@param line string
+  local function parse_line(line)
+    local filename, line_num_str, col_num_str, error_msg = string.match(line, '^(%S+):(%d+):(%d+): (.+)$')
+    if not filename then
+      return
+    end
+    local line_num, col_num = tonumber(line_num_str), tonumber(col_num_str)
+    if line_num == nil or col_num == nil then
+      return
+    end
+    ---@type vim.Diagnostic
+    local diagnostic = {
+      source = 'bazel-go-build',
+      message = error_msg,
+      range = {
+        start = { line = line_num - 1, character = col_num - 1 },
+        ['end'] = { line = line_num - 1, character = col_num - 1 },
+      },
+      severity = vim.diagnostic.severity.ERROR,
+      lnum = line_num - 1,
+      col = col_num - 1,
+    }
+    if not file_diagnostics[filename] then
+      file_diagnostics[filename] = {}
+    end
+    table.insert(file_diagnostics[filename], diagnostic)
+  end
+  for _, line in ipairs(output) do
+    parse_line(line)
+  end
+  local function filename_to_bufnr(filename)
+    local file_uri = vim.uri_from_fname(workspace_root .. '/' .. filename)
+    local bufnr = vim.uri_to_bufnr(file_uri)
+    if not vim.api.nvim_buf_is_loaded(bufnr) then
+      vim.fn.bufload(bufnr)
+    end
+    vim.bo[bufnr].buflisted = true
+    return bufnr
+  end
+
+  local function set_diagnostics()
+    vim.diagnostic.reset(nogo_diagnostic_ns)
+    for filename, diagnostics in pairs(file_diagnostics) do
+      local bufnr = filename_to_bufnr(filename)
+      vim.diagnostic.set(nogo_diagnostic_ns, bufnr, diagnostics, { underline = true })
+      local async = require('plenary.async')
+      async.void(
+        ---@async
+        function()
+          enqueue_next_bazel_go_lint()
+        end
+      )()
+    end
+  end
+  vim.schedule(set_diagnostics)
 end
 
 vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter' }, {
@@ -143,11 +121,19 @@ vim.api.nvim_create_autocmd({ 'BufWritePost', 'BufEnter' }, {
       return
     end
     if #bazel_go_lint_queue == 0 then
-      bazel_go_lint(abs_filepath)
+      local async = require('plenary.async')
+      async.void(
+        ---@async
+        function()
+          bazel_go_lint(abs_filepath)
+        end
+      )()
     end
-    bazel_go_lint_queue[abs_filepath] = function()
-      bazel_go_lint(abs_filepath)
-    end
+    bazel_go_lint_queue[abs_filepath] =
+      ---@async
+      function()
+        bazel_go_lint(abs_filepath)
+      end
   end,
 })
 
--- dot_config/nvim_conf/kickstart.nvim/lua/utils/shell.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/shell.lua
@@ -3,17 +3,6 @@ local Job = require('plenary.job')
 
 M = {}
 
----Run a shell command and throw an error if it fails. Return the output of the command if successful.
----@param cmd string
----@return string
-function M.strict_cmd(cmd)
-  local result = vim.fn.system(cmd)
-  if vim.v.shell_error ~= 0 then
-    error({ message = 'Command failed', cmd = cmd, output = result, error_code = vim.v.shell_error })
-  end
-  return result
-end
-
 ---Run a shell command synchronously and return the output.
 ---@param cmd string
 ---@return boolean, string[]
@@ -31,13 +20,26 @@ M.async_cmd = async.wrap(
   ---@param done fun(success: boolean, output: string[])
   ---@return nil
   function(cmd, args, done)
+    ---@type string[]
+    local output = {}
+    ---@param data string
+    local function handle_output(data)
+      if data then
+        local lines = require('utils.string').split_lines(data)
+        vim.list_extend(output, lines)
+      end
+    end
     Job:new({
       command = cmd,
       args = args,
-      on_exit = function(self, code)
+      on_stdout = function(_, data)
+        handle_output(data)
+      end,
+      on_stderr = function(_, data)
+        handle_output(data)
+      end,
+      on_exit = function(_, code)
         -- TODO: Use on_stderr and on_stdout to make the stderr and stdout come in order when aggregated.
-        local output = vim.list_extend({}, self:result())
-        output = vim.list_extend(output, self:stderr_result())
         done(code == 0, output)
       end,
     }):start()
