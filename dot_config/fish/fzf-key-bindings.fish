19dae5d50cd3970b8de65759e566a59c4fc1ad97
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-09-05 16:48:40 -0700

    🔧 faster ctrl-t for searching files

--- dot_config/fish/fzf-key-bindings.fish
+++ dot_config/fish/fzf-key-bindings.fish
@@ -16,40 +16,25 @@
 # Function is called at the end of the file
 function fzf_key_bindings
 
-    # Store current token in $dir as root for the 'find' command
+    function rg_fzf_files
+        set INITIAL_QUERY $argv
+        set -lx FZF_DEFAULT_OPTS "--height 40% --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS"
+
+        set result (
+            rg --files 2> /dev/null | \
+            fzf --query "$INITIAL_QUERY" \
+                --bind "change:reload:sleep 0.1; rg --files 2> /dev/null | fzf --query {q} || true" \
+                --preview 'bat --style=numbers --color=always --line-range :500 {}' \
+                --preview-window 'right:60%'
+        )
+
+        if test -n "$result"
+            echo $result
+        end
+    end
+
     function fzf-file-widget -d "List files and folders"
-        set -l commandline (__fzf_parse_commandline)
-        set -l dir $commandline[1]
-        set -l fzf_query $commandline[2]
-        set -l prefix $commandline[3]
-
-        # "-path \$dir'*/\\.*'" matches hidden files/folders inside $dir but not
-        # $dir itself, even if hidden.
-        test -n "$FZF_CTRL_T_COMMAND"; or set -l FZF_CTRL_T_COMMAND "
-    command find -L \$dir -mindepth 1 \\( -path \$dir'*/\\.*' -o -fstype 'sysfs' -o -fstype 'devfs' -o -fstype 'devtmpfs' \\) -prune \
-    -o -type f -print \
-    -o -type d -print \
-    -o -type l -print 2> /dev/null | sed 's@^\./@@'"
-
-        test -n "$FZF_TMUX_HEIGHT"; or set FZF_TMUX_HEIGHT 40%
-        begin
-            set -lx FZF_DEFAULT_OPTS "--height $FZF_TMUX_HEIGHT --reverse --bind=ctrl-z:ignore $FZF_DEFAULT_OPTS $FZF_CTRL_T_OPTS"
-            eval "$FZF_CTRL_T_COMMAND | "(__fzfcmd)' -m --query "'$fzf_query'"' | while read -l r
-                set result $result $r
-            end
-        end
-        if [ -z "$result" ]
-            commandline -f repaint
-            return
-        else
-            # Remove last token from commandline.
-            commandline -t ""
-        end
-        for i in $result
-            commandline -it -- $prefix
-            commandline -it -- (string escape $i)
-            commandline -it -- ' '
-        end
+        commandline -i (rg_fzf_files)
         commandline -f repaint
     end
 
