61a458b0d1f527c044430e3dbc3e572650385a1f
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-09-06 11:50:45 -0700

    🔧 create a new branch off the default branch

---
+++ dot_config/fish/functions/gcnm.fish
@@ -0,0 +1,6 @@
+function gcnm --description "Create a new branch of the default branch"
+    set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
+    set -l cmd "git checkout -b $argv $default_branch"
+    echo $cmd
+    eval $cmd
+end
