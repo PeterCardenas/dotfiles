30ec0f6039a15a5a2ffc7fe4f7bb214e411abb3d
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-09-09 18:44:39 -0700

    🔧 try faster default branch before trying correct one

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/git.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/git.lua
@@ -5,7 +5,11 @@ local M = {}
 ---@return boolean, string
 function M.get_default_branch()
   local shell = require('utils.shell')
-  local success, output = shell.async_cmd('fish', { '-c', 'git remote show origin | grep "HEAD branch" | cut -d ":" -f 2 | string trim' })
+  local success, output = shell.async_cmd('fish', { '-c', 'git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@"' })
+  if success then
+    return true, output[1]
+  end
+  success, output = shell.async_cmd('fish', { '-c', 'git remote show origin | grep "HEAD branch" | cut -d ":" -f 2 | string trim' })
   if not success then
     return false, table.concat(output, '\n')
   end
