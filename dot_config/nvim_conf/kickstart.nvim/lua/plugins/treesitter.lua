3919e0fc0d906711e664611a631f2d2909f85c5e
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-10-01 02:12:21 -0700

    🔧 add latex installation

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/treesitter.lua
@@ -12,7 +12,7 @@ return {
     -- See `:help nvim-treesitter`
     require('nvim-treesitter.configs').setup({
       -- Add languages to be installed here that you want installed for treesitter
-      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'markdown', 'fish' },
+      ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'markdown', 'fish', 'latex' },
 
       -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
       auto_install = false,
