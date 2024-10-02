c750766311fb4dd2e7cf77f1aae84c5da6c2534a
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-10-02 11:16:50 -0700

    🧹 remove pcall

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/telescope.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/telescope.lua
@@ -116,6 +116,6 @@ return {
     })
 
     -- Enable telescope fzf native, if installed
-    pcall(require('telescope').load_extension, 'fzf')
+    require('telescope').load_extension('fzf')
   end,
 }
