a90058e2f9b07c7b9020575f1e429957554ef10c
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-10-12 12:40:14 -0700

    🎨 add icon for base tsconfig

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/misc.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/misc.lua
@@ -288,6 +288,7 @@ return {
           ['pyproject.toml'] = { glyph = '', hl = 'MiniIconsCyan' },
           ['.swcrc'] = { glyph = '󰘦', hl = 'MiniIconsYellow' },
           ['tsconfig.json'] = { glyph = '', hl = 'MiniIconsBlue' },
+          ['tsconfig.base.json'] = { glyph = '', hl = 'MiniIconsBlue' },
           ['.npmrc'] = { glyph = '', hl = 'MiniIconsRed' },
           ['package.json'] = { glyph = '', hl = 'MiniIconsRed' },
           ['package-lock.json'] = { glyph = '', hl = 'MiniIconsRed' },
