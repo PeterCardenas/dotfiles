23559b1b59b2823e7185e77946dee53daeb97ed5
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-10-17 20:23:55 -0700

    🔧 more reliably get the default branch

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/git.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/git.lua
@@ -5,11 +5,15 @@ local M = {}
 ---@return boolean, string
 function M.get_default_branch()
   local shell = require('utils.shell')
-  local success, output = shell.async_cmd('fish', { '-c', 'git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@"' })
+  -- Try a local git command first for max speed.
+  local success, output =
+    shell.async_cmd('fish', { '-c', 'git symbolic-ref refs/remotes/origin/HEAD | sed "s@^refs/remotes/origin/@@"; test $pipestatus[1] -eq 0' })
   if success then
     return true, output[1]
   end
-  success, output = shell.async_cmd('fish', { '-c', 'git remote show origin | grep "HEAD branch" | cut -d ":" -f 2 | string trim' })
+  -- Try github cli as a fallback.
+  -- TODO: This requires that a default repo is selected, would be nice to allow selection ad hoc when not set.
+  success, output = shell.async_cmd('gh', { 'repo', 'view', '--json', 'defaultBranchRef', '--jq', '.defaultBranchRef.name' })
   if not success then
     return false, table.concat(output, '\n')
   end
