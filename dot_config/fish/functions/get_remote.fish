61a6b966a44d77e30ab56ce35563889d21c6e950
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-10-19 20:03:52 -0700

    🔧 get remote name and use it in aliases

---
+++ dot_config/fish/functions/get_remote.fish
@@ -0,0 +1,7 @@
+function get_remote
+    set -l remote_name (git rev-parse --abbrev-ref --symbolic-full-name @{u} | sed 's/\/.*//')
+    if test $pipestatus[1] -ne 0
+        set -l remote_name origin
+    end
+    echo $remote_name
+end
