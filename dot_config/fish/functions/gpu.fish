4dbb471e0bf6b8a1ca8a6fa7f7724c77cf826c97
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-10-20 17:21:26 -0700

    🔧 remove remote name from gpu

--- dot_config/fish/functions/gpu.fish
+++ dot_config/fish/functions/gpu.fish
@@ -1,6 +1,5 @@
 function gpu
-    set -l remote_name (get_remote)
-    set -l cmd "git push -u $remote_name $(git branch --show-current)"
+    set -l cmd "git push -u origin $(git branch --show-current)"
     echo $cmd
     eval $cmd
 end
