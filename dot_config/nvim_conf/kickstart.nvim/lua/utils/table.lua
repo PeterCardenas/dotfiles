8929811b4481ea56362c73d7c7899f77afda201e
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-10-26 20:25:44 -0700

    🔧 workaround for treesitter query omnifunc

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/table.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/table.lua
@@ -1,6 +1,6 @@
 local M = {}
 
---- Merges two tables of the same type
+---Merges two tables of the same type
 ---@generic TTable : table
 ---@param table_to_override TTable
 ---@param new_table TTable
@@ -8,6 +8,22 @@ local M = {}
 function M.merge_tables(table_to_override, new_table)
   local merged_table = vim.tbl_extend('force', table_to_override, new_table)
   return merged_table
+end
+
+---Removes duplicates from a list
+---@generic TList : table
+---@param list TList
+---@return TList
+function M.remove_duplicates(list)
+  local seen = {}
+  local new_list = {}
+  for _, v in ipairs(list) do
+    if not seen[v] then
+      table.insert(new_list, v)
+      seen[v] = true
+    end
+  end
+  return new_list
 end
 
 return M
