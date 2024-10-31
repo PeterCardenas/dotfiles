9bf09e37962a4057d1f8f91630197e1e662b0ce1
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-10-30 18:53:23 -0700

    🔧 make copilot chat window open on right

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/sg.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/sg.lua
@@ -20,9 +20,24 @@ return {
       { 'nvim-lua/plenary.nvim' },
     },
     config = function()
+      vim.api.nvim_create_autocmd('BufEnter', {
+        once = true,
+        pattern = 'copilot-chat',
+        callback = function(args)
+          ---@type number
+          local buf = args.bufnr
+          vim.cmd('vert rightbelow wincmd L')
+          -- Get the total width of the Neovim window
+          local total_width = vim.api.nvim_get_option_value('columns', { buf = buf })
+
+          -- Calculate 30% of the total width
+          local split_width = math.floor(total_width * 0.3)
+          vim.cmd('vertical resize ' .. split_width)
+        end,
+      })
       require('CopilotChat').setup({
         window = {
-          layout = 'float',
+          layout = 'vertical',
         },
       })
     end,
