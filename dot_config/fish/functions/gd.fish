1b3e12444a8741af7ea8e0500a2e1f73c0a1763f
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-11-04 20:52:55 -0800

    🔧 fix gd alias

--- dot_config/fish/functions/gd.fish
+++ dot_config/fish/functions/gd.fish
@@ -1,6 +1,10 @@
 function gd
-    echo git diff --numstat \(git merge-base HEAD origin/master\)
-    echo git diff --shortstat \(git merge-base HEAD origin/master\)
-    git diff --numstat (git merge-base HEAD origin/master)
-    git diff --shortstat (git merge-base HEAD origin/master)
+    set -l default_branch (gh repo view --json defaultBranchRef --jq .defaultBranchRef.name)
+    set -l merge_base "$(git merge-base HEAD origin/$default_branch)"
+    set -l numstat_cmd "git diff --numstat $merge_base"
+    set -l shortstat_cmd "git diff --shortstat $merge_base"
+    echo $numstat_cmd | fish_indent --ansi
+    eval $numstat_cmd
+    echo $shortstat_cmd | fish_indent --ansi
+    eval $shortstat_cmd
 end
