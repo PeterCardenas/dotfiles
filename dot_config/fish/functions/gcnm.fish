59cde5d8842ad02e4e5d24304081dfe8be538a98
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-11-04 20:52:30 -0800

    🔧 colorize command output with `fish_indent --ansi`

--- dot_config/fish/functions/gcnm.fish
+++ dot_config/fish/functions/gcnm.fish
@@ -1,6 +1,6 @@
 function gcnm --description "Create a new branch of the default branch"
     set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
     set -l cmd "git checkout -b $argv $default_branch"
-    echo $cmd
+    echo $cmd | fish_indent --ansi
     eval $cmd
 end
--- dot_config/fish/functions/gpm.fish
+++ dot_config/fish/functions/gpm.fish
@@ -1,6 +1,6 @@
 function gpm
     set -l default_branch (git symbolic-ref refs/remotes/origin/HEAD | sed 's@^refs/remotes/origin/@@')
     set -l cmd "git fetch $(get_remote) $default_branch:$default_branch && git rebase $default_branch"
-    echo $cmd
+    echo $cmd | fish_indent --ansi
     eval $cmd
 end
