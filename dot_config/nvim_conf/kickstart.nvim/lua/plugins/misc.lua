c05313ecb5e7008f0314a9fe2bce8ae8eebda4e5
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-12-04 10:47:33 -0800

    🔧 ignore terminal buffers when saving

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/misc.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/misc.lua
@@ -595,6 +595,7 @@ return {
       require('session_manager').setup({
         autoload_mode = require('session_manager.config').AutoloadMode.CurrentDir,
         autosave_ignore_dirs = { '~/', '~/Downloads', '/' },
+        autosave_ignore_buftypes = { 'terminal', 'help', 'quickfix', 'prompt' },
       })
     end,
   },
