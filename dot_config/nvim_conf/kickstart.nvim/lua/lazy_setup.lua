22d7a011724d235605181cdb678711b3aecf6687
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-12-28 04:36:14 -0600

    🔧 load colorscheme when downloading plugins

--- dot_config/nvim_conf/kickstart.nvim/lua/lazy_setup.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/lazy_setup.lua
@@ -31,6 +31,7 @@ require('lazy').setup({
     enabled = false,
     notify = false,
   },
+  install = { colorscheme = { 'tokyonight' } },
   ui = {
     border = 'rounded',
     title = 'Lazy Plugins',
