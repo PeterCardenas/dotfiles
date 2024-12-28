90025713d8f3b134cae9932b3226dba3edd6e582
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2024-12-27 16:00:01 -0800

    ✨ make status/line column background lighter

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/colorscheme.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/colorscheme.lua
@@ -8,9 +8,8 @@ function M.set_focused_colors()
   vim.cmd([[highlight NormalSB guibg=#24283b]])
   vim.cmd([[highlight NormalNC guibg=#24283b]])
   vim.cmd([[highlight NormalFloat guibg=#24283b]])
-  vim.cmd([[highlight FoldColumn guibg=#24283b]])
-  vim.cmd([[highlight SignColumn guibg=#24283b]])
   vim.cmd([[highlight CursorLine guibg=#2a2e40]])
+  vim.api.nvim_set_hl(0, 'TreesitterContextSeparator', { foreground = '#3b4261', background = '#24283b' })
 end
 
 function M.set_unfocused_colors()
@@ -18,9 +17,8 @@ function M.set_unfocused_colors()
   vim.cmd([[highlight NormalSB guibg=#1f2335]])
   vim.cmd([[highlight NormalNC guibg=#1f2335]])
   vim.cmd([[highlight NormalFloat guibg=#1f2335]])
-  vim.cmd([[highlight FoldColumn guibg=#1f2335]])
-  vim.cmd([[highlight SignColumn guibg=#1f2335]])
   vim.cmd([[highlight CursorLine guibg=#1f2335]])
+  vim.api.nvim_set_hl(0, 'TreesitterContextSeparator', { foreground = '#3b4261', background = '#1f2335' })
 end
 
 return M
