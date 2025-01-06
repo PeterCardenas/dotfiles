a1cd8d7787bd6e90300e64cec52f9914c16be6a0
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-01-06 01:43:50 -0600

    ✨ introduce profiling from beginning

--- dot_config/nvim_conf/kickstart.nvim/init.lua
+++ dot_config/nvim_conf/kickstart.nvim/init.lua
@@ -1,3 +1,35 @@
+local profile_env = os.getenv('NVIM_PROFILE')
+if profile_env ~= nil then
+  local profile_startup = profile_env:lower():match('^start$') ~= nil
+  if require('utils.config').USE_SNACKS_PROFILER then
+    local snacks = vim.fn.stdpath('data') .. '/lazy/snacks.nvim'
+    vim.opt.rtp:append(snacks)
+    if profile_startup then
+      require('snacks.profiler').startup({
+        startup = {
+          event = 'VeryLazy',
+        },
+      })
+    end
+  else
+    local profiler = vim.fn.stdpath('data') .. '/lazy/profile.nvim'
+    vim.opt.rtp:append(profiler)
+    require('profile').instrument_autocmds()
+    if profile_startup then
+      require('profile').start('*')
+      vim.api.nvim_create_autocmd('User', {
+        once = true,
+        pattern = 'VeryLazy',
+        callback = function()
+          vim.cmd('ToggleProfile')
+        end,
+      })
+    else
+      require('profile').instrument('*')
+    end
+  end
+end
+
 require('mappings')
 require('options')
 require('lazy_setup')
