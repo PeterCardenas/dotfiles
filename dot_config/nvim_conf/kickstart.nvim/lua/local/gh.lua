2ccef8fa2612a3c059f1adb3635cabb279912916
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-01-11 23:49:20 -0800

    🔧 set gh account automatically

---
+++ dot_config/nvim_conf/kickstart.nvim/lua/local/gh.lua
@@ -0,0 +1,38 @@
+---@async
+local function get_gh_user()
+  local shell = require('utils.shell')
+  local ok, output = shell.async_cmd('git', { 'config', '--get', 'remote.origin.url' })
+  if not ok then
+    return 'PeterCardenas'
+  end
+  local url = output[1]
+  return url:match('^work%-github%.com') ~= nil and 'peter-cardenas-ai' or 'PeterCardenas'
+end
+
+---@async
+local function set_gh_user()
+  local gh_user = get_gh_user()
+  local shell = require('utils.shell')
+  local ok, output = shell.async_cmd('gh', { 'auth', 'token', '--user', gh_user })
+  if not ok then
+    vim.schedule(function()
+      vim.notify(table.concat(output, '\n'), 'error')
+    end)
+  end
+  local gh_token = output[1]
+  vim.schedule(function()
+    vim.env.GH_TOKEN = gh_token
+  end)
+end
+
+-- Set on LazyDone so that plugins are loaded
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'LazyDone',
+  once = true,
+  desc = 'Set up user for gh cli',
+  group = vim.api.nvim_create_augroup('set_gh_user', { clear = true }),
+  callback = function()
+    local async = require('utils.async')
+    async.void(set_gh_user)
+  end,
+})
