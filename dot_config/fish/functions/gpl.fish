3841a98f0164ed0456c47625926dcddeb06cbec5
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-01-11 17:34:25 -0800

    🔧 fix gpl and gpm

--- dot_config/fish/functions/gpl.fish
+++ dot_config/fish/functions/gpl.fish
@@ -1,5 +1,10 @@
 function gpl
-    set -l cmd "git pull $(get_remote) $(git branch --show-current)"
+    set -l current_branch "$(git branch --show-current)"
+    set -l remote "$(git config --get branch.$current_branch.remote)"
+    if test -z "$remote"
+        print_error "No remote configured for $current_branch"
+    end
+    set -l cmd "git pull $remote $current_branch"
     echo $cmd | fish_indent --ansi
     eval $cmd
 end
