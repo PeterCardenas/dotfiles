31bf1074794913beda3622f57a7405241b8303b1
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-01-11 17:01:27 -0800

    🔧 don't make octo buffers dimmed

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/telescope/buffer_picker.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/telescope/buffer_picker.lua
@@ -30,7 +30,7 @@ local function make_buffer_entry()
     local has_error = diagnostics[vim.diagnostic.severity.ERROR] ~= nil and diagnostics[vim.diagnostic.severity.ERROR] > 0
     local Path = require('plenary.path')
     local full_path = Path:new(entry.filename):expand()
-    local is_removed = vim.fn.filereadable(full_path) == 0
+    local is_removed = vim.bo[entry.bufnr].buftype ~= 'acwrite' and vim.fn.filereadable(full_path) == 0
 
     return displayer({
       { entry.indicator, 'DiagnosticWarn' },
