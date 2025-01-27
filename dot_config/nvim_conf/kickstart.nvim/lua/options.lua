922fef8692a4b2da9233fee493045345c9e1b137
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-01-26 16:52:55 -0800

    🔧 fix header filetype detection

--- dot_config/nvim_conf/kickstart.nvim/lua/options.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/options.lua
@@ -98,6 +98,42 @@ vim.filetype.add({
     mdx = 'markdown.mdx',
     swcrc = 'json',
     swiftinterface = 'swift',
+    -- Source: https://github.com/gpanders/dotfiles/blob/master/.config/nvim/filetype.lua
+    h = function(path)
+      -- Try to be a little intelligent when determining if a .h file is C++ or C
+
+      -- If a .cc or .cpp file with the same basename exists next to this
+      -- header file, assume the header is C++
+      local stem = vim.fn.fnamemodify(path, ':r')
+      if vim.uv.fs_stat(string.format('%s.cc', stem)) or vim.uv.fs_stat(string.format('%s.cpp', stem)) then
+        return 'cpp'
+      end
+
+      -- If the header file contains C++ specific keywords, assume it is
+      -- C++
+      if
+        vim.fn.search(
+          string.format(
+            [[\C\%%(%s\)]],
+            table.concat({
+              [[^#include <[^>.]\+>$]],
+              [[\<constexpr\>]],
+              [[\<consteval\>]],
+              [[\<extern "C"\>]],
+              [[^class\> [A-Z]],
+              [[^\s*using\>]],
+              [[\<template\>\s*<]],
+              [[\<std::]],
+            }, '\\|')
+          ),
+          'nw'
+        ) ~= 0
+      then
+        return 'cpp'
+      end
+
+      return 'c'
+    end,
   },
   pattern = {
     ['.*%.bazelrc'] = 'bazelrc',
