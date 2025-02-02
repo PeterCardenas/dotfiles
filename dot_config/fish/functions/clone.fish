fbe26c832939db653e141886c762490a13645473
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-02-02 15:11:02 -0800

    ✨ set user.email and user.signingkey when cloning

--- dot_config/fish/functions/clone.fish
+++ dot_config/fish/functions/clone.fish
@@ -1,13 +1,20 @@
 function clone -d "Clones a repo with ssh alias" -a alias_name -a repo_name
-    set -l ssh_alias
-    switch "$alias_name"
-        case personal
-            set ssh_alias personal-github.com
-        case work
-            set ssh_alias work-github.com
-        case '*'
-            print_error "Invalid alias name: $alias_name"
-            return 1
+    set -l ssh_alias personal-github.com
+    set -l git_dir ".local/share/chezmoi"
+    set -l git_dir "$HOME/$git_dir/.git"
+    set -l repo_matches (string match -gr '.*/(.*)' $repo_name)
+    if test (count $repo_matches) -ne 1
+        print_error "Failed to parse repo name: $repo_name"
+        return 1
     end
+    set -l repo $repo_matches[1]
     git clone "$ssh_alias:$repo_name"
+    if test -d "$git_dir"
+        set -l user_email (git --git-dir="$git_dir" config --get user.email)
+        set -l signing_key (git --git-dir="$git_dir" config --get user.signingkey)
+        git --git-dir="$repo/.git" config --local user.email "$user_email"
+        git --git-dir="$repo/.git" config --local user.signingkey "$signing_key"
+    else
+        print_error "Known git dir for alias $alias_name not found: $git_dir"
+    end
 end
