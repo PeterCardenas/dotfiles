043bbafe849a056b95dba8663c5c8401e5433577
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-02-01 16:23:43 -0800

    🔧 auto switch gh user based on remote url

---
+++ dot_config/fish/functions/gh.fish
@@ -0,0 +1,18 @@
+function gh
+    set -l remote_url (git config --get remote.origin.url)
+    if test -z "$remote_url"
+        print_error "Cannot determine current user with remote: $remote_url"
+        return 1
+    end
+    set -l gh_user PeterCardenas
+    if test -n "$(string match -e 'work-github.com' "$remote_url")"
+        set gh_user peter-cardenas-ai
+    end
+    set -l gh_token (command gh auth token --user $gh_user)
+    if test $status -ne 0
+        print_error "Failed to get gh token for user $gh_user"
+        return 1
+    end
+    print_info "Running for $gh_user"
+    env GH_TOKEN=$gh_token gh $argv
+end
