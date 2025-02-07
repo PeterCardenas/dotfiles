53f2573b3c82ab1b00e459553d8173acbd0aa0cb
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-02-06 19:23:43 -0800

    🔧 better `gf` for sh files

--- dot_config/nvim_conf/kickstart.nvim/lua/options.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/options.lua
@@ -93,7 +93,7 @@ vim.api.nvim_set_hl(0, '@markup.link.label.markdown', { fg = '#2ac3de', underdot
 local filetyp_options_group = vim.api.nvim_create_augroup('FiletypeOptions', { clear = true })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'conf',
+  pattern = { 'conf', 'sh' },
   group = filetyp_options_group,
   callback = function()
     -- Remove prefixed '/' from includeexpr
