c00c733ee6378d38e0c67e3d98cf13604130aac1
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-02-09 22:19:28 -0800

    ✨ add python auto importing

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/lsp.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/lsp.lua
@@ -34,4 +34,43 @@ if vim.fn.has('mac') == 1 then
   table.insert(M.FT_WITH_LSP, 'swift')
 end
 
+--- Applies the given defaults to the completion item, modifying it in place.
+---
+--- @param item lsp.CompletionItem
+--- @param defaults lsp.ItemDefaults?
+local function apply_defaults(item, defaults)
+  if not defaults then
+    return
+  end
+
+  item.insertTextFormat = item.insertTextFormat or defaults.insertTextFormat
+  item.insertTextMode = item.insertTextMode or defaults.insertTextMode
+  item.data = item.data or defaults.data
+  if defaults.editRange then
+    local textEdit = item.textEdit or {}
+    item.textEdit = textEdit
+    textEdit.newText = textEdit.newText or item.textEditText or item.insertText
+    if defaults.editRange.start then
+      textEdit.range = textEdit.range or defaults.editRange
+    elseif defaults.editRange.insert then
+      textEdit.insert = defaults.editRange.insert
+      textEdit.replace = defaults.editRange.replace
+    end
+  end
+end
+
+---@param result vim.lsp.CompletionResult
+---@return lsp.CompletionItem[]
+function M.completion_result_to_items(result)
+  if result.items then
+    for _, item in ipairs(result.items) do
+      ---@diagnostic disable-next-line: param-type-mismatch
+      apply_defaults(item, result.itemDefaults)
+    end
+    return result.items
+  else
+    return result
+  end
+end
+
 return M
