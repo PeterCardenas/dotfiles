9081cf28afa0c370cad8653be26ee88f581ae129
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-03-09 00:24:53 -0800

    ✨ various improvements for setup.sh to be one-shot and retriable

--- setup.sh
+++ setup.sh
@@ -15,14 +15,17 @@ function install_ghostty() {
 		zig build -p "$HOME/.local" -Doptimize=ReleaseFast
 		popd || exit 1
 	elif [ "$(uname)" == "Darwin" ]; then
-		gh release --repo ghostty-org/ghostty download tip --pattern ghostty-macos-universal.zip
+		# TODO install ghostty with personal fork
+		gh release --repo ghostty-org/ghostty download tip --pattern ghostty-macos-universal.zip --clobber
+		sudo rm -rf /Applications/Ghostty.app
 		unzip ghostty-macos-universal.zip -d /Applications
+		rm ghostty-macos-universal.zip
 	fi
 }
 
 function setup_ubuntu() {
-	sudo apt update -y
-	sudo apt install -y nala
+	sudo -B apt update -y
+	sudo -B apt install -y nala
 	dpkg_packages=(
 		curl
 		wget
@@ -55,8 +58,8 @@ function setup_ubuntu() {
 		# For generating compile_commands.json for ccls
 		bear
 	)
-	sudo nala remove -y xsel
-	sudo nala install -y "${dpkg_packages[@]}"
+	sudo -B nala remove -y xsel
+	sudo -B nala install -y "${dpkg_packages[@]}"
 
 	# Setup third-party repositories
 	# Docker
@@ -69,22 +72,20 @@ function setup_ubuntu() {
 	)
 	curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 	chmod a+r /etc/apt/keyrings/docker.asc
-	echo \
-		"deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
-		sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+	echo \n "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \n	$(. /etc/os-release && echo "$VERSION_CODENAME") stable" |
+		sudo -B tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 	# Fish shell
 	dpkg_third_party+=(
 		fish
 	)
-	sudo apt-add-repository ppa:fish-shell/release-4
+	sudo -B apt-add-repository ppa:fish-shell/release-4
 
 	# Git
 	dpkg_third_party+=(
 		git
 	)
-	sudo add-apt-repository ppa:git-core/ppa
+	sudo -B add-apt-repository ppa:git-core/ppa
 
 	# GitHub CLI
 	dpkg_third_party+=(
@@ -105,10 +106,10 @@ function setup_ubuntu() {
 	dpkg_third_party+=(
 		speedtest
 	)
-	curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash
+	curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo -B bash
 
-	sudo nala update -y
-	sudo nala install -y "${dpkg_third_party[@]}"
+	sudo -B nala update -y
+	sudo -B nala install -y "${dpkg_third_party[@]}"
 
 	# Setup golang
 	wget https://go.dev/dl/go1.22.4.linux-amd64.tar.gz
@@ -116,15 +117,71 @@ function setup_ubuntu() {
 	rm go1.22.4.linux-amd64.tar.gz
 }
 
-function setup_mac() {
-	# Install MacPorts
-	curl -O https://github.com/macports/macports-base/releases/download/v2.9.3/MacPorts-2.9.3.tar.bz2
+function setup_macos_defaults() {
+	IS_AUTOHIDE="$(defaults read com.apple.dock autohide)"
+	defaults write com.apple.dock autohide -bool true
+	defaults write com.apple.dock show-recents -bool false
+	# Do not want to override already set docked apps
+	if [ "$IS_AUTOHIDE" == "1" ]; then
+		defaults write com.apple.dock persistent-apps -array
+	fi
+	killall Dock
+	# TODO: menubar: battery percent show, show seconds in clock, hide spotlight, siri
+	# TODO: key repeat
+	# TODO: tap to click
+}
+
+function install_macports() {
+	if command -v port >/dev/null 2>&1; then
+		echo "MacPorts already installed."
+		return 0
+	fi
+	if [ -n "$(stat -q /opt/local/bin)" ]; then
+		echo "Adding port to PATH"
+		export PATH="/opt/local/bin:$PATH"
+		return 0
+	fi
+	echo "Installing MacPorts"
+	curl -LO https://github.com/macports/macports-base/releases/download/v2.9.3/MacPorts-2.9.3.tar.bz2
 	tar xvjf MacPorts-2.9.3.tar.bz2
 	pushd MacPorts-2.9.3
-	./configure && make && sudo make install
+	./configure
+	make
+	sudo -B make install
 	popd
 	rm -rf MacPorts-2.9.3*
-	sudo port -v selfupdate
+}
+
+function setup_mac() {
+	setup_macos_defaults
+	if ! gcc --version >/dev/null 2>&1; then
+		echo "Accepting XCode license"
+		sudo -B xcodebuild -license accept
+	fi
+	XCODE_INSTALLED="$(
+		xcode-select -p 1>/dev/null
+		echo $?
+	)"
+	if [ "$XCODE_INSTALLED" == "2" ]; then
+		xcode-select --install
+		read -p "Press enter once finished"
+	fi
+	install_macports
+
+	echo "Ensuring MacPorts version and port tree"
+	sudo mkdir -p /opt/local/etc/macports
+	sudo touch /opt/local/etc/macports/sources.conf
+	sudo tee "/opt/local/etc/macports/sources.conf" >/dev/null <<EOF
+rsync://bos.us.rsync.macports.org/macports/release/tarballs/ports.tar [default]
+EOF
+
+	mkdir -p $HOME/.macports
+	touch $HOME/.macports/macports.conf
+	cat <<EOF >"$HOME/.macports/macports.conf"
+rsync_server="atl.us.rsync.macports.org"
+rsync_dir="MacPorts/release/tarballs/base.tar"
+EOF
+	sudo -B port -v selfupdate
 
 	# Install MacPorts packages
 	ports=(
@@ -143,19 +200,23 @@ function setup_mac() {
 		# Used for 3rd/image.nvim plugin
 		imagemagick
 		# For lazy.nvim
-		luarocks
+		lua-luarocks lua51
 		# MacOS/iOS development in neovim
 		xcode-build-server
 		# MacOS bash version is wildly out of date
 		bash
 		# Build tmux and other c/cpp programs
 		automake
+		# tmux dependencies
+		libevent libutf8proc
 		# Generate compile_commands.json for ccls
 		bear
 	)
-	sudo port install "${ports[@]}"
+	# TODO: figure out how to actually skip interactive questions
+	sudo -B port install -N "${ports[@]}"
 
 	# Install ccls
+	# TODO: Move later since it's less important and takes a while
 	pushd $HOME/thirdparty
 	wget https://github.com/llvm/llvm-project/releases/download/llvmorg-18.1.8/clang+llvm-18.1.8-arm64-apple-macos11.tar.xz
 	tar xzvf clang+llvm-18.1.8-arm64-apple-macos11.tar.xz
@@ -169,34 +230,72 @@ function setup_mac() {
 }
 
 function setup_unix() {
-	# Install pnpm
-	curl -fsSL https://get.pnpm.io/install.sh | sh -
+	mkdir -p $HOME/.local/share
+	GROUP="$(id -gn)"
+	sudo chown $USER:$GROUP $HOME/.local/share
+	source $HOME/.zshrc
+	if ! command -v pnpm >/dev/null 2>&1; then
+		# Install pnpm
+		curl -fsSL https://get.pnpm.io/install.sh | sh -
+		source $HOME/.zshrc
+	fi
 	# Install Node via pnpm
 	pnpm env use --global lts
 
 	# Install zig version manager
-	curl https://raw.githubusercontent.com/tristanisham/zvm/master/install.sh | bash
+	if ! command -v zvm >/dev/null 2>&1; then
+		curl https://raw.githubusercontent.com/tristanisham/zvm/master/install.sh | bash
+		source $HOME/.zshrc
+	fi
 
+	mkdir -p $HOME/.local/bin
+	export PATH="$PATH:$HOME/.local/bin"
 	# Install Starship prompt
-	curl -sS https://starship.rs/install.sh | sh -s -- -y
+	if ! command -v starship >/dev/null 2>&1; then
+		curl -sS https://starship.rs/install.sh | sh -s -- -y --bin-dir=$HOME/.local/bin
+	fi
+
+	# Install tmux
+	# TODO: check if current version is correct as well
+	if ! command -v tmux >/dev/null 2>&1; then
+		pushd $HOME/thirdparty
+		wget https://github.com/tmux/tmux/releases/download/3.5a/tmux-3.5a.tar.gz
+		tar xvzf tmux-3.5a.tar.gz
+		pushd tmux-3.5a
+		./configure --prefix=$HOME/.local --enable-utf8proc
+		make
+		make install
+		popd
+		popd
+	fi
 
 	# Setup tmux plugin manager (tpm)
+	rm -rf ~/.tmux/plugins/tpm
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-	~/.tmux/plugins/tpm/bin/install_plugins
 
 	# Change login shell to fish
-	sudo chsh -s /usr/bin/fish
+	FISH_LOCATION="$(which fish)"
+	if ! grep -q "$FISH_LOCATION" /etc/shells; then
+		echo "$FISH_LOCATION" | sudo tee -a /etc/shells
+	fi
+	chsh -s "$FISH_LOCATION"
 
 	# Install chezmoi
-	sh -c "$(curl -fsLS get.chezmoi.io)"
-	chezmoi completion fish >~/.config/fish/completions/chezmoi.fish
+	pushd "$HOME"
+	sh -c "$(curl -fsLS get.chezmoi.io/lb)"
+	popd
 
 	# Install fzf
+	rm -rf ~/.fzf
 	git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 	~/.fzf/install --xdg --no-bash --no-zsh --no-key-bindings --no-update-rc --no-completion
 
 	# Setup rust
-	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	# TODO: we should always use rustup, currently fish is pulling in rust in macports, need to resolve
+	if ! command -v cargo >/dev/null 2>&1; then
+		curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+	fi
+	export PATH="$PATH:$HOME/.cargo/bin"
 
 	# Install cargo binstall
 	curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
@@ -218,9 +317,6 @@ function setup_unix() {
 	# --features isn't supported by cargo binstall
 	cargo install --features 'pcre2' ripgrep
 
-	# Add aichat completions
-	curl -L https://github.com/sigoden/aichat/raw/refs/heads/main/scripts/completions/aichat.fish -o ~/.config/fish/completions/aichat.fish
-
 	# Setup delta/bat theme
 	mkdir -p "$(bat --config-dir)/themes"
 	pushd "$(bat --config-dir)/themes" || exit 1
@@ -231,52 +327,69 @@ function setup_unix() {
 	# Install go packages
 	go install github.com/jesseduffield/lazygit@latest
 	go install golang.org/x/lint/golint@latest
-	go install github.co/iximiuz/cdebug@latest
-
-	# Manually cloned tooling
-	mkdir -p "$HOME/thirdparty"
-	pushd "$HOME/thirdparty" || exit 1
-
-	# Install tmux
-	wget https://github.com/tmux/tmux/releases/download/3.4/tmux-3.4.tar.gz
-	tar xvzf tmux-3.4.tar.gz
-	pushd tmux-3.4 || exit
-	./configure && make && sudo make install
-	popd || exit 1
-
-	# Exit thirdparty to $HOME
-	popd || exit 1
+	go install github.com/iximiuz/cdebug@latest
 
 	# Install kitty and kitten cli
-	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin
+	curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin launch=n
 
-	# Clone and apply dotfiles
-	cat <<EOF >"$HOME/.ssh/config"
+	if [ ! -f "$HOME/.ssh/config" ]; then
+		mkdir -p "$HOME/.ssh"
+		touch "$HOME/.ssh/config"
+		# Clone and apply dotfiles
+		cat <<EOF >"$HOME/.ssh/config"
 Host personal-github.com
  	HostName github.com
 	User git
 	IdentityFile ~/.ssh/id_ed25519_personal
 EOF
-	ssh-keygen -t ed25519 -C "111733365+PeterCardenas@users.noreply.github.com" -f "$HOME/.ssh/id_ed25519_personal"
-	gh auth login --git-protocol ssh --hostname github.com
-	gh ssh-key add "$HOME/.ssh/id_ed25519_personal.pub" --title "Automated ssh key upload"
+	fi
+	if [ ! -f "$HOME/.ssh/id_ed25519_personal" ]; then
+		# TODO: use real/noreply email to not have ties with github
+		ssh-keygen -t ed25519 -C "111733365+PeterCardenas@users.noreply.github.com" -f "$HOME/.ssh/id_ed25519_personal"
+	fi
+	if ! gh auth status; then
+		gh auth login --git-protocol ssh --hostname github.com --skip-ssh-key
+		# TODO: use bitwarden for storing a shared ssh key
+		gh ssh-key add "$HOME/.ssh/id_ed25519_personal.pub" --title "Automated ssh key upload"
+	fi
 	chezmoi init --apply personal-github.com:PeterCardenas/dotfiles.git
+	chezmoi git lfs install
+	chezmoi git lfs pull
+	chezmoi apply
 
 	# Post-dotfiles setup
 	fish -c "fisher update"
-	bob sync
-	cp ~/fonts/* ~/.fonts
+	export BOB_CONFIG=$HOME/.config/bob/config.json
+	fish -c "vswitch kickstart.nvim"
+	# TODO: consider symlinking instead of copying
+	if [ "$(uname)" == "Linux" ]; then
+		mkdir -p $HOME/.fonts
+		cp $HOME/fonts/* $HOME/.fonts/
+	elif [ "$(uname)" == "Darwin" ]; then
+		mkdir -p $HOME/Library/Fonts
+		cp $HOME/fonts/* $HOME/Library/Fonts/
+	fi
 	fc-cache -f -v
-
+	$HOME/.tmux/plugins/tpm/bin/install_plugins
+	chezmoi completion fish >~/.config/fish/completions/chezmoi.fish
 	install_ghostty
+
+	# Add aichat completions
+	curl -L https://github.com/sigoden/aichat/raw/refs/heads/main/scripts/completions/aichat.fish -o ~/.config/fish/completions/aichat.fish
 
 	rg --generate=complete-fish >"$HOME/.config/fish/completions/rg.fish"
 	bob complete fish >"$HOME/.config/fish/completions/bob.fish"
 
 	# Add some autogenerated completions
-	fish_update_completions
+	fish -c "fish_update_completions"
+	# TODO install nvim plugins, mason deps, treesitter deps
+	# TODO: setup gpg
+	# TODO: setup gnome
+	# TODO: reboot
 }
 
+# TODO: install zen browser and setup configuration, maybe (always allow x to open) things?
+mkdir -p $HOME/thirdparty
 if [ "$(uname)" == "Linux" ]; then
 	setup_ubuntu
 elif [ "$(uname)" == "Darwin" ]; then
