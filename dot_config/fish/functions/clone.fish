e80132f5ec4b0da61a0399b04fda6dae41d78019
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-03-09 03:17:21 -0700

    🔧 allow .git suffix when cloning with clone

--- dot_config/fish/functions/clone.fish
+++ dot_config/fish/functions/clone.fish
@@ -9,11 +9,24 @@ function clone -d "Clones a repo with ssh alias" -a alias_name -a repo_name
     end
     set -l repo $repo_matches[1]
     git clone "$ssh_alias:$repo_name"
+    if test $status -ne 0
+        print_error "Failed to clone $repo_name"
+        return 1
+    end
+    set repo_matches (string match -gr '(.*)\.git' $repo)
+    if test (count $repo_matches) -eq 1
+        set repo $repo_matches[1]
+    end
+    cd "$repo"
+    if test $status -ne 0
+        print_error "No repo at $repo"
+        return 1
+    end
     if test -d "$git_dir"
         set -l user_email (git --git-dir="$git_dir" config --get user.email)
         set -l signing_key (git --git-dir="$git_dir" config --get user.signingkey)
-        git --git-dir="$repo/.git" config --local user.email "$user_email"
-        git --git-dir="$repo/.git" config --local user.signingkey "$signing_key"
+        git config --local user.email "$user_email"
+        git config --local user.signingkey "$signing_key"
     else
         print_error "Known git dir for alias $alias_name not found: $git_dir"
     end
