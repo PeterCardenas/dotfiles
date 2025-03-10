ea2fb6e1ea819056ccd894c262a34dc748ad2b1e
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-03-10 16:22:56 -0700

    🔧 fix setup_fork for recent `gh`
    recent update in `gh` requires a default repo to fork so now we set the
    default repo before calling `gh fork`

--- dot_config/fish/functions/setup_fork.fish
+++ dot_config/fish/functions/setup_fork.fish
@@ -1,13 +1,7 @@
 function setup_fork -d "Sets up a fork with ssh alias and sets default repo"
-    gh repo fork --clone --remote
-    if test $status -ne 0
-        print_error "Failed to fork"
-        return 1
-    end
-    set -l origin_url (git config --get remote.origin.url)
-    set -l fork_url (git config --get remote.upstream.url)
-    if test -z "$fork_url" -o -z "$origin_url"
-        print_error "Failed to get remote url(s). upstream url: $fork_url, origin url: $origin_url"
+    set -l fork_url (git config --get remote.origin.url)
+    if test -z "$fork_url"
+        print_error "Failed to get fork url for remote origin: $fork_url"
         return 1
     end
     set -l fork_matches (string match -gr '(.*github.com):(.*)' $fork_url)
@@ -21,6 +15,17 @@ function setup_fork -d "Sets up a fork with ssh alias and sets default repo"
     if test (count $repo_matches) -eq 1
         set fork_repo $repo_matches[1]
     end
+    gh repo set-default $fork_repo
+    gh repo fork --clone --remote
+    if test $status -ne 0
+        print_error "Failed to fork"
+        return 1
+    end
+    set -l origin_url (git config --get remote.origin.url)
+    if test -z "$origin_url"
+        print_error "Failed to get remote url(s). upstream url: $fork_url, origin url: $origin_url"
+        return 1
+    end
     set -l origin_matches (string match -gr 'git@github.com:(.*)' $origin_url)
     if test (count $origin_matches) -ne 1
         print_error "Failed to parse origin url: $origin_url"
@@ -28,5 +33,4 @@ function setup_fork -d "Sets up a fork with ssh alias and sets default repo"
     end
     set -l origin_repo $origin_matches[1]
     git remote set-url origin "$ssh_alias:$origin_repo"
-    gh repo set-default $fork_repo
 end
