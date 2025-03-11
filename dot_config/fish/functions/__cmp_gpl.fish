b5de30c74c77ff1e3d45ea05482057b8cbf4d46f
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-03-10 19:11:05 -0700

    🔧 fix gpl when no remote is set

--- dot_config/fish/functions/__cmp_gpl.fish
+++ dot_config/fish/functions/__cmp_gpl.fish
@@ -1,8 +1,10 @@
 function __cmp_gpl
     set -l current_branch "$(git branch --show-current)"
     set -l remote "$(git config --get branch.$current_branch.remote)"
+    # TODO: Sometimes remote is not set, so we should check where the ref is.
+    # For now default to origin.
     if test -z "$remote"
-        return
+        set remote origin
     end
     set -l cmd "git pull $remote $current_branch"
     echo $cmd
