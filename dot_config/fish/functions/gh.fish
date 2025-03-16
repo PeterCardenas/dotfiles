e0ce560b6f5b6d4fdb01f1fe674947a1baef2824
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-03-15 17:46:41 -0700

    🧹 fail gh wrapper less often
    tried to run gh outside of a git repo and always failed, no fail less

--- dot_config/fish/functions/gh.fish
+++ dot_config/fish/functions/gh.fish
@@ -1,12 +1,20 @@
 function gh
-    set -l remote_url (git config --get remote.origin.url)
-    if test -z "$remote_url"
-        print_error "Cannot determine current user with remote: $remote_url"
-        return 1
+    if not git rev-parse --is-inside-work-tree >/dev/null 2>&1
+        set -l default_user (command gh api user --jq '.login' 2>/dev/null)
+        if test $status -eq 0
+            print_warn "Not in a git repository, using default user: $default_user"
+        else
+            print_warn "Not in a git repository"
+        end
+        command gh $argv
+        return
     end
+    set -l remote_url (git config --get remote.origin.url)
     set -l gh_user PeterCardenas
     if test -n "$(string match -e 'work-github.com' "$remote_url")"
         set gh_user peter-cardenas-ai
+    else if test -z "$(string match -e 'personal-github.com' "$remote_url")"
+        print_warn "GitHub user could not be determined from remote URL: $remote_url, using default user: $gh_user"
     end
     set -l gh_token (command gh auth token --user $gh_user)
     if test $status -ne 0
