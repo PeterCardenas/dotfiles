9f2c94b4f8963ad48df20bbc34494667a4d24a3d
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-03-25 13:22:11 -0700

    🔧 add completions for ai function

--- dot_config/fish/functions/ai.fish
+++ dot_config/fish/functions/ai.fish
@@ -1,4 +1,4 @@
-function ai --wraps aichat -a session_name
+function ai -a session_name
     set -l prompt $argv[2..-1]
     aichat --prompt "Keep responses short. Avoid making long lists and if there are multiple options only output at most 3 with very short descriptions" --save-session --session $session_name $prompt
 end
