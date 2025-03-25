71e250c8baee1152f994c13ff3e6641392dcce61
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-03-24 17:32:58 -0700

    🧹 fix type error for sessionoptions

--- dot_config/nvim_conf/kickstart.nvim/lua/options.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/options.lua
@@ -98,6 +98,7 @@ vim.g.clipboard = {
   },
 }
 
+---@type string[]
 local current_sessionoptions = vim.opt.sessionoptions:get()
 table.insert(current_sessionoptions, 'globals')
 current_sessionoptions = vim
