6299c5d7b65e9581b40c756075bca82cc4c3eeae
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-04-12 03:28:24 -0700

    🔧 fix aichat alias completions

--- dot_config/fish/completions/ai.fish
+++ dot_config/fish/completions/ai.fish
@@ -3,7 +3,7 @@ complete -f -c ai
 
 function __ai_session_names
     set sessions_dir "$HOME/.config/aichat/sessions"
-    set session_names (ls --color=none -1 $sessions_dir | string trim --right --chars '.yaml')
+    set session_names (ls --color=none -1 $sessions_dir | string replace --regex '\.yaml$' '')
     for session_name in $session_names
         set -l yaml_file "$sessions_dir/$session_name.yaml"
         if not test -d "$yaml_file"; and test -f "$yaml_file"
