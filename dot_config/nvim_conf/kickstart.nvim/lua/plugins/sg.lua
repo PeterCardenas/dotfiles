1852e69ce56f5d26b735dd024d163adfd72d3ae6
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-04-23 03:58:39 -0700

    ✨ add code companion + azure openai support

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/sg.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/sg.lua
@@ -1,5 +1,15 @@
 local Config = require('utils.config')
-local Finder = require('plugins.telescope.setup')
+
+---@param filepath string
+---@return boolean, string
+local function read_api_key(filepath)
+  local api_key_filepath = vim.fn.expand(filepath)
+  local api_key_lines = vim.fn.readfile(api_key_filepath) ---@type string[]
+  if #api_key_lines == 0 or api_key_lines[1] == '' then
+    return false, ''
+  end
+  return true, api_key_lines[1]
+end
 
 ---@type LazyPluginSpec[]
 return {
@@ -60,24 +70,33 @@ return {
       'echasnovski/mini.icons',
     },
     config = function()
-      local anthropic_key_filepath = vim.fn.expand('~/.local/share/anthropic/api_key')
-      local anthropic_key_lines = vim.fn.readfile(anthropic_key_filepath) ---@type string[]
-      if #anthropic_key_lines == 0 or anthropic_key_lines[1] == '' then
-        vim.notify('Unable to load avante.nvim, Anthropic API key not found at ' .. anthropic_key_filepath, vim.log.levels.ERROR)
+      local anthropic_key_ok, anthropic_key = read_api_key('~/.local/share/anthropic/api_key')
+      if not anthropic_key_ok then
+        vim.notify('Unable to load avante.nvim, Anthropic API key not found', vim.log.levels.ERROR)
         return
       end
-      ---@type string
-      vim.env.ANTHROPIC_API_KEY = anthropic_key_lines[1]
-      local brave_search_key_filepath = vim.fn.expand('~/.local/share/brave_search/api_key')
-      ---@type boolean, string[]
-      local brave_search_key_ok, brave_search_key_lines = pcall(vim.fn.readfile, brave_search_key_filepath)
-      if not brave_search_key_ok or #brave_search_key_lines == 0 or brave_search_key_lines[1] == '' then
-        vim.notify('Brave Search API key not found at ' .. brave_search_key_filepath, vim.log.levels.ERROR)
+      vim.env.ANTHROPIC_API_KEY = anthropic_key
+      local brave_search_key_ok, brave_search_key = read_api_key('~/.local/share/brave_search/api_key')
+      if not brave_search_key_ok then
+        vim.notify('Brave Search API key not found', vim.log.levels.ERROR)
       else
-        vim.env.BRAVE_API_KEY = brave_search_key_lines[1]
+        vim.env.BRAVE_API_KEY = brave_search_key
+      end
+      local azure_openai_key_ok, azure_openai_key = read_api_key('~/.local/share/azure/api_key')
+      if not azure_openai_key_ok then
+        vim.notify('Azure OpenAI API key not found', vim.log.levels.ERROR)
+      else
+        vim.env.AZURE_OPENAI_API_KEY = azure_openai_key
+      end
+      local azure_embedding_key_ok, azure_embedding_key = read_api_key('~/.local/share/azure/embedding_key')
+      if not azure_embedding_key_ok then
+        vim.notify('Azure Embedding API key not found', vim.log.levels.ERROR)
+      else
+        vim.env.OPENAI_API_KEY = azure_embedding_key
       end
 
       local AvanteToolsHelpers = require('avante.llm_tools.helpers')
+      local provider = vim.env.AZURE_OPENAI_API_KEY and 'azure' or 'claude'
       -- TODO: Properly respect gitignore for repo map
       -- TODO: building repo map should be async
       -- TODO: diffs should join with relevant diffs next to them
@@ -88,6 +107,8 @@ return {
         hints = {
           enabled = false,
         },
+        provider = provider,
+        auto_suggestions_provider = provider,
         behaviour = {
           auto_suggestions = not Config.USE_SUPERMAVEN,
           -- TODO: Use this when it's fast and less buggy
@@ -96,6 +117,29 @@ return {
           auto_apply_diff_after_generation = false,
           enable_claude_text_editor_tool_mode = false,
           enable_token_counting = false,
+        },
+        -- rag_service = {
+        --   enabled = true,
+        --   embed_model = 'text-embedding-3-large',
+        --   endpoint = 'https://eastus.api.cognitive.microsoft.com/openai/deployments/text-embedding-3-large-1/',
+        -- },
+        azure = {
+          endpoint = 'https://westus.api.cognitive.microsoft.com/',
+
+          -- model = 'gpt-4o',
+          -- deployment = 'gpt-4o-2024-08-06',
+          -- api_version = '2024-02-01',
+          -- max_tokens = 16384,
+
+          -- model = 'gpt-4o-mini',
+          -- deployment = 'gpt-4o-mini-2024-07-18',
+          -- api_version = '2024-02-01',
+          -- max_tokens = 16384,
+
+          model = 'o3-mini',
+          deployment = 'o3-mini-2025-01-31',
+          max_completion_tokens = 16384,
+          reasoning_effort = 'low',
         },
         web_search_engine = {
           provider = 'brave',
@@ -178,6 +222,47 @@ return {
         },
         selector = {
           provider = 'fzf_lua',
+        },
+        mappings = {
+          suggestion = {
+            accept = not Config.USE_SUPERMAVEN and '<C-y>' or '<M-l>',
+          },
+        },
+      })
+    end,
+  },
+  {
+    'olimorris/codecompanion.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+    cmd = { 'CodeCompanion', 'CodeCompanionCmd', 'CodeCompanionChat', 'CodeCompanionActions' },
+    config = function()
+      require('codecompanion').setup({
+        adapters = {
+          azure_openai = function()
+            return require('codecompanion.adapters').extend('azure_openai', {
+              env = {
+                api_key = 'AZURE_OPENAI_API_KEY',
+                endpoint = 'https://westus.api.cognitive.microsoft.com/',
+              },
+              schema = {
+                model = {
+                  default = 'gpt-4o-mini-2024-07-18',
+                  api_version = '2024-02-01',
+                },
+              },
+            })
+          end,
+        },
+        strategies = {
+          chat = {
+            adapter = 'azure_openai',
+          },
+          inline = {
+            adapter = 'azure_openai',
+          },
         },
       })
     end,
