921753604f6f7bb70e9cc5583be35580dfc6c5eb
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-04-25 03:33:31 -0700

    🔧 do not crash profile.nvim when enabling first time

--- dot_config/nvim_conf/kickstart.nvim/init.lua
+++ dot_config/nvim_conf/kickstart.nvim/init.lua
@@ -16,19 +16,21 @@ if profile_env ~= nil then
     end
   else
     local profiler = vim.fn.stdpath('data') .. '/lazy/profile.nvim'
-    vim.opt.rtp:append(profiler)
-    require('profile').instrument_autocmds()
-    if profile_startup then
-      require('profile').start('*')
-      vim.api.nvim_create_autocmd('User', {
-        once = true,
-        pattern = 'VeryLazy',
-        callback = function()
-          vim.cmd('ToggleProfile')
-        end,
-      })
-    else
-      require('profile').instrument('*')
+    if vim.fn.isdirectory(profiler) == 1 then
+      vim.opt.rtp:append(profiler)
+      require('profile').instrument_autocmds()
+      if profile_startup then
+        require('profile').start('*')
+        vim.api.nvim_create_autocmd('User', {
+          once = true,
+          pattern = 'VeryLazy',
+          callback = function()
+            vim.cmd('ToggleProfile')
+          end,
+        })
+      else
+        require('profile').instrument('*')
+      end
     end
   end
 end
