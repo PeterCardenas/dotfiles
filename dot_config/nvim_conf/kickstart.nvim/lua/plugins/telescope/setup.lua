436ac45a1879c0414a6478c6fea6cd8a80c847b5
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-05-08 18:21:50 -0700

    📜 add todo for find words in ignored files

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/telescope/setup.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/telescope/setup.lua
@@ -89,6 +89,7 @@ function M.find_words(show_ignore)
       end,
     })
   else
+    -- TODO: Add show_ignore being true doesn't work here.
     require('fzf-lua.providers.grep').live_grep_glob({
       ---@type string
       rg_opts = M.rg_words_opts(show_ignore),
