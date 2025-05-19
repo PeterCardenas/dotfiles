3d0766dbceeba4830d5e0a35bff5a84636c79a4a
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-05-18 17:24:10 -0700

    🔧 allow opening multiple files in lazygit

--- dot_config/fish/functions/lazygit_nvim.fish
+++ dot_config/fish/functions/lazygit_nvim.fish
@@ -1,4 +1,13 @@
-function lazygit_nvim -a file_or_dir -a line
+function lazygit_nvim
+    set -l file_or_dir
+    set -l line
+    for arg in $argv
+        if test -f $arg; or test -d $arg
+            set file_or_dir $file_or_dir $arg
+        else
+            set line $arg
+        end
+    end
     if test -z "$NVIM"
         if test -z "$line"
             nvim -- $file_or_dir
