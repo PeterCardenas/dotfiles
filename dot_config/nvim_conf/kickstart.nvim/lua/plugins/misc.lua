e39a2548a6938e4e412dc1f96bf765fcd2aa8f6f
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-05-29 23:59:15 -0700

    🔧 do not show math symbol images

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/misc.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/misc.lua
@@ -1034,6 +1034,9 @@ return {
             'svg',
           },
           enabled = Config.USE_SNACKS_IMAGE,
+          math = {
+            enabled = false,
+          },
           resolve = function(file, src)
             if File.file_exists(src) then
               return src
