b846775d7389f085bf9ce3cfa119b480b5bafaa1
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-06-25 01:54:48 -0700

    ✨ conceal whole line comments

--- dot_config/nvim_conf/kickstart.nvim/after/queries/html/highlights.scm
+++ dot_config/nvim_conf/kickstart.nvim/after/queries/html/highlights.scm
@@ -1,6 +1,5 @@
 ; extends
 
 ; conceal comments for octo.nvim
-; TODO: Add a custom directive to check if the comment is the only content on the line, in which case a conceal_line is used.
 ((comment) @conceal
-  (#set! conceal ""))
+  (#maybe-conceal-whole-line! @conceal))
