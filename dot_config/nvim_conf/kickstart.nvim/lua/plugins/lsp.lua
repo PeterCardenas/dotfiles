c3692e0fb0bf29316d847a8cdcca878490272672
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-07-10 01:54:13 -0700

    🧹 clean up lsp config setup

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/lsp.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/lsp.lua
@@ -113,250 +113,264 @@ return {
   },
   ft = Lsp.FT_WITH_LSP,
   config = function()
+    ---@type string
+    vim.env.PATH = vim.env.PATH .. ':' .. vim.fn.stdpath('data') .. '/mason/bin'
+    local project_root = File.get_git_root() or File.get_cwd()
+
+    ---@type table<string, LspTogglableConfig>
+    local servers = {}
+
     ---@type fun(path: string): string?
     local get_clangd_root = require('lspconfig.util').root_pattern('compile_commands.json')
     local clangd_root = get_clangd_root(vim.fn.expand('%:p:h'))
     local home = os.getenv('HOME')
     local clangd_enabled = clangd_root ~= nil and home ~= nil
-    ---@type string
-    vim.env.PATH = vim.env.PATH .. ':' .. vim.fn.stdpath('data') .. '/mason/bin'
-    local pnpm_home = os.getenv('PNPM_HOME')
-    local tsserver_lib = pnpm_home ~= nil and pnpm_home .. '/global/5/node_modules/typescript/lib' or ''
+    servers.clangd = {
+      enabled = clangd_enabled and Config.USE_CLANGD,
+      offset_encoding = 'utf-16',
+      filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
+      capabilities = {
+        offsetEncoding = { 'utf-16' },
+        general = {
+          positionEncodings = { 'utf-16' },
+        },
+      },
+      cmd = {
+        'clangd',
+        '--header-insertion=never',
+        '--query-driver=**',
+        '--background-index',
+        '--clang-tidy',
+        '--completion-style=detailed',
+        '--function-arg-placeholders',
+        '--fallback-style=llvm',
+      },
+      init_options = {
+        usePlaceholders = true,
+        completeUnimported = true,
+        clangdFileStatus = true,
+      },
+    }
+
+    servers.gopls = gopls_config()
+
+    servers.rust_analyzer = { enabled = false }
+
+    -- Prefer to use nogo, but there is not language server for it yet.
+    servers.golangci_lint_ls = {
+      enabled = false,
+      init_options = {
+        command = { 'golangci-lint', 'run', '--output.json.path', 'stdout', '--default', 'none', '--enable', 'errcheck,ineffassign,unused' },
+      },
+    }
+
+    servers.eslint = {
+      cmd_env = {
+        NODE_OPTIONS = '--max-old-space-size=8192',
+      },
+    }
+
+    servers.stylelint_lsp = {
+      filetypes = { 'css', 'scss' },
+      settings = {
+        stylelintplus = {
+          autoFixOnFormat = true,
+        },
+      },
+    }
+
     local lua_ls_path = 'lua-language-server'
     local tip_lua_ls_path = vim.fn.expand('~/projects/lua-language-server/bin/lua-language-server')
     if File.file_exists(tip_lua_ls_path) and Config.USE_LUA_LS_TIP then
       lua_ls_path = tip_lua_ls_path
     end
-    -- Enable the following language servers
-    -- Type inferred from https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-    ---@type table<string, LspTogglableConfig>
-    local servers = {
-      clangd = {
-        enabled = clangd_enabled and Config.USE_CLANGD,
-        offset_encoding = 'utf-16',
-        filetypes = { 'c', 'cpp', 'objc', 'objcpp', 'cuda' },
-        capabilities = {
-          offsetEncoding = { 'utf-16' },
-          general = {
-            positionEncodings = { 'utf-16' },
+    servers.lua_ls = {
+      enabled = not Config.USE_RUST_LUA_LS,
+      cmd = { lua_ls_path },
+      settings = {
+        Lua = {
+          workspace = {
+            checkThirdParty = false,
           },
-        },
-        cmd = {
-          'clangd',
-          '--header-insertion=never',
-          '--query-driver=**',
-          '--background-index',
-          '--clang-tidy',
-          '--completion-style=detailed',
-          '--function-arg-placeholders',
-          '--fallback-style=llvm',
-        },
-        init_options = {
-          usePlaceholders = true,
-          completeUnimported = true,
-          clangdFileStatus = true,
-        },
-      },
-      gopls = gopls_config(),
-      rust_analyzer = { enabled = false },
-      -- Prefer to use nogo, but there is not language server for it yet.
-      golangci_lint_ls = {
-        enabled = false,
-        init_options = {
-          command = { 'golangci-lint', 'run', '--output.json.path', 'stdout', '--default', 'none', '--enable', 'errcheck,ineffassign,unused' },
-        },
-      },
-      eslint = {
-        cmd_env = {
-          NODE_OPTIONS = '--max-old-space-size=8192',
-        },
-      },
-      stylelint_lsp = {
-        filetypes = { 'css', 'scss' },
-        settings = {
-          stylelintplus = {
-            autoFixOnFormat = true,
-          },
-        },
-      },
-      lua_ls = {
-        enabled = not Config.USE_RUST_LUA_LS,
-        cmd = { lua_ls_path },
-        settings = {
-          Lua = {
-            workspace = {
-              checkThirdParty = false,
+          telemetry = { enable = false },
+          hint = { enable = true, arrayIndex = 'Disable' },
+          diagnostics = {
+            severity = {
+              ['await-in-sync'] = 'Error',
+              ['no-unknown'] = 'Warning',
             },
-            telemetry = { enable = false },
-            hint = { enable = true, arrayIndex = 'Disable' },
-            diagnostics = {
-              severity = {
-                ['await-in-sync'] = 'Error',
-                ['no-unknown'] = 'Warning',
-              },
-              neededFileStatus = {
-                ['await-in-sync'] = 'Opened',
-                ['no-unknown'] = 'Any',
-              },
-              globals = {
-                'require',
-              },
-              unusedLocalExclude = { '_*' },
+            neededFileStatus = {
+              ['await-in-sync'] = 'Opened',
+              ['no-unknown'] = 'Any',
             },
+            globals = {
+              'require',
+            },
+            unusedLocalExclude = { '_*' },
           },
         },
       },
-      bzl = {
-        enabled = false,
-        filetypes = { 'bzl' },
-      },
-      mdx_analyzer = {
-        init_options = {
-          typescript = {
-            tsdk = tsserver_lib,
-          },
+    }
+
+    servers.bzl = {
+      enabled = false,
+      filetypes = { 'bzl' },
+    }
+
+    local pnpm_home = os.getenv('PNPM_HOME')
+    local tsserver_lib = pnpm_home ~= nil and pnpm_home .. '/global/5/node_modules/typescript/lib' or ''
+    servers.mdx_analyzer = {
+      init_options = {
+        typescript = {
+          tsdk = tsserver_lib,
         },
       },
-      jsonls = {
-        filetypes = { 'json', 'jsonc' },
-        settings = {
-          json = {
-            validate = { enable = true },
-            schemas = {
-              {
-                fileMatch = { 'package.json' },
-                url = 'https://json.schemastore.org/package.json',
-              },
-              {
-                fileMatch = { 'tsconfig.json', 'tsconfig.*.json' },
-                url = 'https://json.schemastore.org/tsconfig.json',
-              },
-              {
-                fileMatch = { 'pyrightconfig.json' },
-                url = 'https://raw.githubusercontent.com/microsoft/pyright/main/packages/vscode-pyright/schemas/pyrightconfig.schema.json',
-              },
-              {
-                fileMatch = { '.vscode/settings.json', 'vscode-settings.json' },
-                url = 'https://github.com/wraith13/vscode-schemas/raw/master/en/latest/schemas/settings/user.json',
-              },
-              {
-                fileMatch = { 'vscode-keybindings.json' },
-                url = 'https://github.com/wraith13/vscode-schemas/raw/master/en/latest/schemas/keybindings.json',
-              },
-              {
-                fileMatch = { '.vscode/launch.json' },
-                url = 'https://github.com/wraith13/vscode-schemas/raw/master/en/latest/schemas/launch.json',
-              },
-              {
-                fileMatch = { '.vscode/tasks.json' },
-                url = 'https://github.com/wraith13/vscode-schemas/raw/master/en/latest/schemas/tasks.json',
-              },
-              {
-                fileMatch = { '.vscode/extensions.json' },
-                url = 'https://github.com/wraith13/vscode-schemas/raw/master/en/latest/schemas/extensions.json',
-              },
-              {
-                fileMatch = { 'swc.json', 'swc.*.json', '.swcrc', '.*.swcrc' },
-                url = 'https://swc.rs/schema.json',
-              },
-              {
-                fileMatch = { '.luarc.json' },
-                url = 'https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json',
-              },
-              {
-                fileMatch = { '.emmyrc.json' },
-                url = 'https://github.com/CppCXY/emmylua-analyzer-rust/raw/refs/heads/main/resources/schema.json',
-              },
+    }
+
+    servers.jsonls = {
+      filetypes = { 'json', 'jsonc' },
+      settings = {
+        json = {
+          validate = { enable = true },
+          schemas = {
+            {
+              fileMatch = { 'package.json' },
+              url = 'https://json.schemastore.org/package.json',
+            },
+            {
+              fileMatch = { 'tsconfig.json', 'tsconfig.*.json' },
+              url = 'https://json.schemastore.org/tsconfig.json',
+            },
+            {
+              fileMatch = { 'pyrightconfig.json' },
+              url = 'https://raw.githubusercontent.com/microsoft/pyright/main/packages/vscode-pyright/schemas/pyrightconfig.schema.json',
+            },
+            {
+              fileMatch = { '.vscode/settings.json', 'vscode-settings.json' },
+              url = 'https://github.com/wraith13/vscode-schemas/raw/master/schemas/json/settings.json',
+            },
+            {
+              fileMatch = { 'vscode-keybindings.json' },
+              url = 'https://github.com/wraith13/vscode-schemas/raw/master/schemas/json/keybindings.json',
+            },
+            {
+              fileMatch = { '.vscode/launch.json' },
+              url = 'https://github.com/wraith13/vscode-schemas/raw/master/schemas/json/launch.json',
+            },
+            {
+              fileMatch = { '.vscode/tasks.json' },
+              url = 'https://github.com/wraith13/vscode-schemas/raw/master/schemas/json/tasks.json',
+            },
+            {
+              fileMatch = { '.vscode/extensions.json' },
+              url = 'https://github.com/wraith13/vscode-schemas/raw/master/schemas/json/extensions.json',
+            },
+            {
+              fileMatch = { 'swc.json', 'swc.*.json', '.swcrc', '.*.swcrc' },
+              url = 'https://swc.rs/schema.json',
+            },
+            {
+              fileMatch = { '.luarc.json' },
+              url = 'https://raw.githubusercontent.com/LuaLS/vscode-lua/master/setting/schema.json',
+            },
+            {
+              fileMatch = { '.emmyrc.json' },
+              url = 'https://github.com/CppCXY/emmylua-analyzer-rust/raw/refs/heads/main/resources/schema.json',
             },
           },
         },
       },
-      yamlls = {
-        filetypes = { 'yaml' },
-        settings = {
-          yaml = {
-            ['schemaStore.enable'] = true,
-          },
-        },
-        handlers = {
-          -- [LspMethod.textDocument_publishDiagnostics] = function() end,
+    }
+
+    servers.yamlls = {
+      filetypes = { 'yaml' },
+      settings = {
+        yaml = {
+          ['schemaStore.enable'] = true,
         },
       },
-      taplo = {},
-      vale_ls = {
-        filetypes = { 'markdown', 'text', 'dosini', 'yaml', 'markdown.mdx' },
-        root_dir = function(bufnr, cb)
-          local filename = vim.api.nvim_buf_get_name(bufnr)
-          local root_path = File.get_ancestor_dir('.vale.ini', filename)
-          cb(root_path)
-        end,
-        single_file_support = false,
-      },
-      zls = {
-        handlers = {
-          ---@param error lsp.ResponseError
-          ---@param result lsp.PublishDiagnosticsParams
-          ---@param ctx lsp.HandlerContext
-          [LspMethod.textDocument_publishDiagnostics] = function(error, result, ctx)
-            -- TODO: Fix these duplicated diagnostics upstream in zls
-            -- Create a map from diagnostic location to diagnostic messages
-            local loc_to_msgs = {} ---@type table<string, string[]>
+    }
 
-            -- Process diagnostics to create location-to-message mapping and deduplicate
-            local deduplicated_diagnostics = {} ---@type lsp.Diagnostic[]
-            for _, diagnostic in ipairs(result.diagnostics) do
-              -- Create a unique key for the location
-              local loc_key = string.format(
-                '%s:%d:%d-%d:%d',
-                result.uri,
-                diagnostic.range.start.line,
-                diagnostic.range.start.character,
-                diagnostic.range['end'].line,
-                diagnostic.range['end'].character
-              )
+    servers.taplo = {}
 
-              -- Initialize the message array if it doesn't exist
-              if not loc_to_msgs[loc_key] then
-                loc_to_msgs[loc_key] = {}
-              end
+    servers.vale_ls = {
+      filetypes = { 'markdown', 'text', 'dosini', 'yaml', 'markdown.mdx' },
+      root_dir = function(bufnr, cb)
+        local filename = vim.api.nvim_buf_get_name(bufnr)
+        local root_path = File.get_ancestor_dir('.vale.ini', filename)
+        cb(root_path)
+      end,
+      single_file_support = false,
+    }
 
-              -- Check if this message already exists for this location
-              local message_exists = false
-              for _, existing_msg in ipairs(loc_to_msgs[loc_key]) do
-                if existing_msg == diagnostic.message then
-                  message_exists = true
-                  break
-                end
-              end
+    servers.zls = {
+      handlers = {
+        ---@param error lsp.ResponseError
+        ---@param result lsp.PublishDiagnosticsParams
+        ---@param ctx lsp.HandlerContext
+        [LspMethod.textDocument_publishDiagnostics] = function(error, result, ctx)
+          -- TODO: Fix these duplicated diagnostics upstream in zls
+          -- Create a map from diagnostic location to diagnostic messages
+          local loc_to_msgs = {} ---@type table<string, string[]>
 
-              -- Add the message if it's not a duplicate
-              if not message_exists then
-                loc_to_msgs[loc_key][#loc_to_msgs[loc_key] + 1] = diagnostic.message
-                deduplicated_diagnostics[#deduplicated_diagnostics + 1] = diagnostic
+          -- Process diagnostics to create location-to-message mapping and deduplicate
+          local deduplicated_diagnostics = {} ---@type lsp.Diagnostic[]
+          for _, diagnostic in ipairs(result.diagnostics) do
+            -- Create a unique key for the location
+            local loc_key = string.format(
+              '%s:%d:%d-%d:%d',
+              result.uri,
+              diagnostic.range.start.line,
+              diagnostic.range.start.character,
+              diagnostic.range['end'].line,
+              diagnostic.range['end'].character
+            )
+
+            -- Initialize the message array if it doesn't exist
+            if not loc_to_msgs[loc_key] then
+              loc_to_msgs[loc_key] = {}
+            end
+
+            -- Check if this message already exists for this location
+            local message_exists = false
+            for _, existing_msg in ipairs(loc_to_msgs[loc_key]) do
+              if existing_msg == diagnostic.message then
+                message_exists = true
+                break
               end
             end
 
-            -- Replace the diagnostics with the deduplicated list
-            result.diagnostics = deduplicated_diagnostics
-            return vim.lsp.diagnostic.on_publish_diagnostics(error, result, ctx)
-          end,
-        },
-        settings = {
-          zls = {
-            enable_build_on_save = true,
-            build_on_save_args = { '-Doptimize=ReleaseFast', '-j4' },
-          },
+            -- Add the message if it's not a duplicate
+            if not message_exists then
+              loc_to_msgs[loc_key][#loc_to_msgs[loc_key] + 1] = diagnostic.message
+              deduplicated_diagnostics[#deduplicated_diagnostics + 1] = diagnostic
+            end
+          end
+
+          -- Replace the diagnostics with the deduplicated list
+          result.diagnostics = deduplicated_diagnostics
+          return vim.lsp.diagnostic.on_publish_diagnostics(error, result, ctx)
+        end,
+      },
+      settings = {
+        zls = {
+          enable_build_on_save = true,
+          build_on_save_args = { '-Doptimize=ReleaseFast', '-j4' },
         },
       },
-      vimls = {},
-      glsl_analyzer = {},
-      buf_ls = {
-        enabled = false,
-      },
-      bashls = {},
     }
-    local python_lsp_config = Python.python_lsp_config()
-    servers = Table.merge_tables(servers, python_lsp_config)
+
+    servers.vimls = {}
+
+    servers.glsl_analyzer = {}
+
+    servers.buf_ls = {
+      enabled = false,
+    }
+
+    servers.bashls = {}
+
+    Python.add_config(servers)
 
     -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
     local capabilities ---@type lsp.ClientCapabilities
@@ -368,9 +382,9 @@ return {
     end
 
     -- Setup language servers found locally.
-    LocalLsp.setup(capabilities)
+    LocalLsp.add_config(servers)
     -- Setup specific autocmds and ruff_lsp.
-    Python.setup(capabilities)
+    Python.setup()
 
     for server_name, server_config in pairs(servers) do
       if server_config.enabled ~= false then
