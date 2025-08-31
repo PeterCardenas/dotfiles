f1bed60f915f1351e784833eca63e492cea2e09b
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-08-30 17:08:37 -0700

    🧹 generalize clone function

--- dot_config/fish/completions/clone.fish
+++ dot_config/fish/completions/clone.fish
@@ -1,4 +1,2 @@
 # Remove files as completions.
 complete -f -c clone
-
-complete -f -c clone -n '__fish_is_arg_eq_nth 1' -a 'personal work' -d 'ssh alias'
--- dot_config/fish/functions/clone.fish
+++ dot_config/fish/functions/clone.fish
@@ -1,40 +1,6 @@
 function clone -d "Clones a repo with ssh alias" -a alias_name -a repo_name
-    set -l ssh_alias personal-github.com
-    set -l git_dir ".local/share/chezmoi"
-    set -l user PeterCardenas
-    set -l git_dir "$HOME/$git_dir/.git"
-    set -l repo_matches (string match -gr '.*/(.*)' $repo_name)
-    if test (count $repo_matches) -ne 1
-        print_error "Failed to parse repo name: $repo_name"
-        return 1
-    end
-    set -l repo $repo_matches[1]
-    git clone "$ssh_alias:$repo_name" $argv[3..-1]
-    if test $status -ne 0
-        print_error "Failed to clone $repo_name"
-        return 1
-    end
-    set repo_matches (string match -gr '(.*)\.git' $repo)
-    if test (count $repo_matches) -eq 1
-        set repo $repo_matches[1]
-    end
-    cd "$repo"
-    if test $status -ne 0
-        print_error "No repo at $repo"
-        return 1
-    end
-    if test -d "$git_dir"
-        set -l user_email (git --git-dir="$git_dir" config --get user.email)
-        set -l signing_key (git --git-dir="$git_dir" config --get user.signingkey)
-        git config --local user.email "$user_email"
-        git config --local user.signingkey "$signing_key"
-    else
-        print_error "Known git dir for alias $alias_name not found: $git_dir"
-        return 1
-    end
-    set -l existing_fork (gh repo list --fork --json parent --jq ".[] | .parent.owner.login + \"/\" + .parent.name | select(. == \"$user/$repo\")")
-    if test (count $existing_fork) -eq 1
-        print_info "Setting up existing fork"
-        setup_fork
-    end
+    set ssh_alias personal-github.com
+    set git_dir ".local/share/chezmoi"
+    set user PeterCardenas
+    clone-common $ssh_alias $git_dir $user "$alias_name" "$repo_name"
 end
