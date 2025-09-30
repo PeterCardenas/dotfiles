309d360b10d7435b6bcd4569a5da41a08a5bc3b1
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-09-30 07:08:52 -0700

    ✨ add regex highlighting for concatentated strings

---
+++ dot_config/nvim_conf/kickstart.nvim/after/queries/lua/injections.scm
@@ -0,0 +1,18 @@
+; extends
+
+; still add regex highlight when concatenating, only does one level though
+(function_call
+  (method_index_expression
+    method: (identifier) @_method
+    (#any-of? @_method "find" "match" "gmatch" "gsub"))
+  arguments: (arguments
+    .
+    (binary_expression
+      left: (string
+        content: (string_content) @injection.content
+        (#set! injection.language "luap")
+        (#set! injection.include-children))?
+      right: (string
+        content: (string_content) @injection.content
+        (#set! injection.language "luap")
+        (#set! injection.include-children))?)))
