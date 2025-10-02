47f9ab410adbfa9e1698924c6ba2625ebdbda085
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-10-02 00:18:01 -0700

    🔧 try to guard against out of bounds errors

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/treesitter.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/treesitter.lua
@@ -31,13 +31,16 @@ end
 ---@param col? integer
 ---@return TSNode?, vim.treesitter.LanguageTree?, vim.treesitter.Query?
 local function get_ts_info(bufnr, row, col)
-  if (row and (not bufnr or row < 1 or row > vim.api.nvim_buf_line_count(bufnr))) or (col and col < 1) then
+  if (row and (not bufnr or row < 0 or row > vim.api.nvim_buf_line_count(bufnr))) or (col and col < 0) then
     return nil, nil, nil
   end
-  local node_under_cursor = vim.treesitter.get_node({
+  local success, node_under_cursor = pcall(vim.treesitter.get_node, {
     bufnr = bufnr,
     pos = bufnr and { row, col },
   })
+  if not success then
+    return nil, nil, nil
+  end
   local parser = vim.treesitter.get_parser(nil, nil, { error = false })
   if not parser or not node_under_cursor then
     return node_under_cursor, parser, nil
@@ -67,13 +70,19 @@ function M.inside_comment_block(bufnr, row, col)
   cursor_row = cursor_row - 1
   row = row or cursor_row
   col = col or cursor_col
-  for id, node, _ in query:iter_captures(node_under_cursor, 0, row, row + 1) do
-    local capture = query.captures[id]
-    if capture:find('comment') and is_in_node_range(node, row, col) then
-      return true
+  local success, inside_comment_block = pcall(function()
+    for id, node, _ in query:iter_captures(node_under_cursor, bufnr or 0, row, row + 1) do
+      local capture = query.captures[id]
+      if capture:find('comment') and is_in_node_range(node, row, col) then
+        return true
+      end
     end
+    return false
+  end)
+  if not success then
+    return false
   end
-  return false
+  return inside_comment_block
 end
 
 ---@return boolean
