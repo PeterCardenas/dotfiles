1d606109d73603d6fd137bf3d8c7dbfc5bb049a2
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-10-05 08:05:52 -0700

    🧹 remove deprecated -o

--- dot_config/fish/completion_utils.fish
+++ dot_config/fish/completion_utils.fish
@@ -1,5 +1,5 @@
 function __fish_is_arg_eq_nth
-    set tokens (commandline -poc)
+    set tokens (commandline -xpc)
     set token_str (string replace -ra '(^|\-).+?\s+' '' -- $tokens)
     set token_str (string replace -ra '\s+' ' ' -- $token_str)
     set tokens (string split " " "$token_str")
@@ -10,7 +10,7 @@ function __fish_is_arg_eq_nth
 end
 
 function __fish_needs_command -a command
-    set tokens (commandline -opc)
+    set tokens (commandline -xpc)
     if test (count $tokens) -eq 1; and test $tokens[1] = $command
         return 0
     end
@@ -18,7 +18,7 @@ function __fish_needs_command -a command
 end
 
 function __fish_is_token_ge_nth
-    set tokens (commandline -poc)
+    set tokens (commandline -xpc)
     if test (count $tokens) -ge $argv[1]
         return 0
     end
