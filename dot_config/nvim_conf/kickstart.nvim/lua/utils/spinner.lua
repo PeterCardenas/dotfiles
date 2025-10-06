8c382b2cb441641f7beec5b0323e3ee8174d1bdb
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-10-05 21:36:58 -0700

    🔧 start bazel go lint and add loading spinner

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/spinner.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/spinner.lua
@@ -12,15 +12,30 @@ function M.create_spinner(pattern)
   end
 end
 
----@return { start: (fun(cb: (fun(): nil)): nil), stop: (fun(): nil) }
+---@class SpinnerTimer
+---@field start fun(cb: (fun(): nil)): nil
+---@field stop fun(): nil
+
+---@return SpinnerTimer
 function M.create_timer()
   local timer = vim.uv.new_timer()
+  local is_cleared = false
   return {
     ---@param cb fun(): nil
     start = function(cb)
-      timer:start(0, 16, vim.schedule_wrap(cb))
+      timer:start(
+        0,
+        16,
+        vim.schedule_wrap(function()
+          if is_cleared then
+            return
+          end
+          cb()
+        end)
+      )
     end,
     stop = function()
+      is_cleared = true
       timer:stop()
       timer:close()
     end,
