0c1ec8615cab695fa3d4c59404163458c67dfae8
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-10-05 21:23:59 -0700

    🔧 try out gpt 5

--- dot_config/nvim_conf/kickstart.nvim/lua/plugins/sg.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/plugins/sg.lua
@@ -86,12 +86,12 @@ return {
       'echasnovski/mini.icons',
     },
     config = function()
-      local anthropic_key_ok, anthropic_key = read_api_key('~/.local/share/anthropic/api_key')
-      if not anthropic_key_ok then
-        vim.notify('Unable to load avante.nvim, Anthropic API key not found', vim.log.levels.ERROR)
-        return
-      end
-      vim.env.ANTHROPIC_API_KEY = anthropic_key
+      -- local anthropic_key_ok, anthropic_key = read_api_key('~/.local/share/anthropic/api_key')
+      -- if not anthropic_key_ok then
+      --   vim.notify('Unable to load avante.nvim, Anthropic API key not found', vim.log.levels.ERROR)
+      --   return
+      -- end
+      -- vim.env.ANTHROPIC_API_KEY = anthropic_key
       local brave_search_key_ok, brave_search_key = read_api_key('~/.local/share/brave_search/api_key')
       if not brave_search_key_ok then
         vim.notify('Brave Search API key not found', vim.log.levels.ERROR)
@@ -101,9 +101,11 @@ return {
       local azure_openai_key_ok, azure_openai_key = read_api_key('~/.local/share/azure/api_key')
       if not azure_openai_key_ok then
         vim.notify('Azure OpenAI API key not found', vim.log.levels.ERROR)
-      else
-        vim.env.AZURE_OPENAI_API_KEY = azure_openai_key
       end
+      -- local azure_openai_gpt_5_key_ok, azure_openai_gpt_5_key = read_api_key('~/.local/share/azure/gpt_5_api_key')
+      -- if not azure_openai_gpt_5_key_ok then
+      --   vim.notify('Azure OpenAI GPT 5 API key not found', vim.log.levels.ERROR)
+      -- end
       local azure_embedding_key_ok, azure_embedding_key = read_api_key('~/.local/share/azure/embedding_key')
       if not azure_embedding_key_ok then
         vim.notify('Azure Embedding API key not found', vim.log.levels.ERROR)
@@ -112,7 +114,6 @@ return {
       end
 
       local AvanteToolsHelpers = require('avante.llm_tools.helpers')
-      local provider = vim.env.AZURE_OPENAI_API_KEY and 'azure' or 'claude'
       -- TODO: Properly respect gitignore for repo map
       -- TODO: building repo map should be async
       -- TODO: diffs should join with relevant diffs next to them
@@ -131,7 +132,7 @@ return {
           },
         },
         provider = 'claude-code',
-        auto_suggestions_provider = provider,
+        auto_suggestions_provider = 'azure',
         behaviour = {
           auto_suggestions = not Config.USE_SUPERMAVEN,
           -- TODO: Use this when it's fast and less buggy
@@ -160,11 +161,12 @@ return {
         },
         providers = {
           azure = {
-            endpoint = 'https://westus.api.cognitive.microsoft.com/',
             parse_api_key = function()
-              return vim.env.AZURE_OPENAI_API_KEY
+              return azure_openai_key
+              -- return azure_openai_gpt_5_key
             end,
 
+            endpoint = 'https://westus.api.cognitive.microsoft.com/',
             model = 'gpt-4o',
             deployment = 'gpt-4o-2024-08-06',
 
@@ -173,6 +175,11 @@ return {
 
             -- model = 'o3-mini',
             -- deployment = 'o3-mini-2025-01-31',
+
+            -- endpoint = 'https://eastus2.api.cognitive.microsoft.com/',
+            -- model = 'gpt-5',
+            -- deployment = 'gpt-5-2025-08-07',
+
             -- Make smaller than max (128k) because token count calculation is undershooting
             context_window = 110000,
             extra_request_body = {
