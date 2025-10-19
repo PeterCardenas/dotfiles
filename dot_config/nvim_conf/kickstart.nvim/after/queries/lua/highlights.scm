3f61b7855f35e6196104f07e93c4ecf8c8c26f6d
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-10-19 15:06:13 -0700

    🔧 highlight two concatenations of regex strings

--- dot_config/nvim_conf/kickstart.nvim/after/queries/lua/highlights.scm
+++ dot_config/nvim_conf/kickstart.nvim/after/queries/lua/highlights.scm
@@ -1,6 +1,6 @@
 ; extends
 
-; still add regex highlight when concatenating, only does one level though
+; still add regex highlight when concatenating, currently handling two concatenations
 (function_call
   (method_index_expression
     method: (identifier) @_method
@@ -12,3 +12,18 @@
         content: (string_content) @string.regexp)?
       right: (string
         content: (string_content) @string.regexp)?)))
+
+(function_call
+  (method_index_expression
+    method: (identifier) @_method
+    (#any-of? @_method "find" "match" "gmatch" "gsub"))
+  arguments: (arguments
+    .
+    (binary_expression
+      left: (string
+        content: (string_content) @string.regexp)?
+      right: (binary_expression
+        left: (string
+          content: (string_content) @string.regexp)?
+        right: (string
+          content: (string_content) @string.regexp)?))))
