67050319c52e6b1d0ff513186814923640980603
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-10-19 12:48:51 -0700

    🔧 ignore fetching message

--- dot_config/nvim_conf/kickstart.nvim/lua/local/lazygit.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/local/lazygit.lua
@@ -31,7 +31,7 @@ local function setup_lazygit_buffer()
         end
       end
       vim.keymap.set({ 't' }, 'q', function()
-        local last_line_content = vim.api.nvim_buf_get_lines(bufnr, -2, -1, false)[1]
+        local last_line_content = vim.api.nvim_buf_get_lines(bufnr, -2, -1, false)[1]:gsub('^Fetching%s.%s', '')
         -- Don't navigate away when typing in new branch name or searching.
         if last_line_content:match('^Search:') or last_line_content:match('^Confirm:') or last_line_content:match('^Filter:') then
           vim.api.nvim_feedkeys('q', 'n', true)
