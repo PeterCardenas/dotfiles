c3b12eb846833e352597e91af3680742611abc5d
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-12-26 14:56:54 -0800

    🔧 handle command prefix hackily

--- dot_config/nvim_conf/kickstart.nvim/after/queries/fish/injections.scm
+++ dot_config/nvim_conf/kickstart.nvim/after/queries/fish/injections.scm
@@ -31,3 +31,41 @@
   ; remove when following issue is resolved: https://github.com/ram02z/tree-sitter-fish/issues/31
   (#offset! @injection.content 0 1 0 -1)
   (#set! injection.language "jq"))
+
+(command
+  name: (word) @_cmd
+  (#eq? @_cmd "command")
+  .
+  argument: (word) @_gh_arg
+  (#eq? @_gh_arg "gh")
+  .
+  argument: (word) @_api_arg
+  (#eq? @_api_arg "api")
+  .
+  argument: (word) @_graphql_arg
+  (#eq? @_graphql_arg "graphql")
+  argument: (word) @_field_query_flag
+  (#eq? @_field_query_flag "-f")
+  .
+  argument: (concatenation
+    (word) @_query_flag
+    (#eq? @_query_flag "query=")
+    .
+    (single_quote_string) @injection.content
+    ; remove when following issue is resolved: https://github.com/ram02z/tree-sitter-fish/issues/31
+    (#offset! @injection.content 0 1 0 -1)
+    (#set! injection.language "graphql")))
+
+(command
+  name: (word) @_cmd
+  (#eq? @_cmd "command")
+  .
+  argument: (word) @_gh_arg
+  (#eq? @_gh_arg "gh")
+  argument: (word) @_field_query_flag
+  (#eq? @_field_query_flag "--jq")
+  .
+  argument: (single_quote_string) @injection.content
+  ; remove when following issue is resolved: https://github.com/ram02z/tree-sitter-fish/issues/31
+  (#offset! @injection.content 0 1 0 -1)
+  (#set! injection.language "jq"))
