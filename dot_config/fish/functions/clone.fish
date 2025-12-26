916a924d4663b7e7909d38b4d59320109eaf5b85
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2025-12-26 13:33:12 -0800

    🧹 remove unnecessary user arg

--- dot_config/fish/functions/clone.fish
+++ dot_config/fish/functions/clone.fish
@@ -1,6 +1,5 @@
 function clone -d "Clones a repo" -a repo_name
     set ssh_alias personal-github.com
     set git_dir ".local/share/chezmoi"
-    set user PeterCardenas
-    clone-common $ssh_alias $git_dir $user "$repo_name" $argv[2..-1]
+    clone-common $ssh_alias $git_dir "$repo_name" $argv[2..-1]
 end
