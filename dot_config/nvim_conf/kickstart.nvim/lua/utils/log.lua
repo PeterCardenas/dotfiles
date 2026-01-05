6863f12f6bffc1966b2a13dae0682950a0ea487c
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-01-05 04:34:55 -0800

    🧹 add title support to log.notify

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/log.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/log.lua
@@ -3,12 +3,16 @@
 
 local M = {}
 
+---@class NotifyOpts
+---@field title? string
+
 ---Safely notify, scheduling if in a fast event
 ---@param msg string
 ---@param level vim.log.levels
-local function safe_notify(msg, level)
+---@param opts? NotifyOpts
+local function safe_notify(msg, level, opts)
   local function do_notify()
-    vim.notify(msg, level)
+    vim.notify(msg, level, opts)
   end
 
   if vim.in_fast_event() then
@@ -20,32 +24,37 @@ end
 
 ---Log a trace message
 ---@param msg string
-function M.notify_trace(msg)
-  safe_notify(msg, vim.log.levels.TRACE)
+---@param opts? NotifyOpts
+function M.notify_trace(msg, opts)
+  safe_notify(msg, vim.log.levels.TRACE, opts)
 end
 
 ---Log a debug message
 ---@param msg string
-function M.notify_debug(msg)
-  safe_notify(msg, vim.log.levels.DEBUG)
+---@param opts? NotifyOpts
+function M.notify_debug(msg, opts)
+  safe_notify(msg, vim.log.levels.DEBUG, opts)
 end
 
 ---Log an info message
 ---@param msg string
-function M.notify_info(msg)
-  safe_notify(msg, vim.log.levels.INFO)
+---@param opts? NotifyOpts
+function M.notify_info(msg, opts)
+  safe_notify(msg, vim.log.levels.INFO, opts)
 end
 
 ---Log a warning message
 ---@param msg string
-function M.notify_warn(msg)
-  safe_notify(msg, vim.log.levels.WARN)
+---@param opts? NotifyOpts
+function M.notify_warn(msg, opts)
+  safe_notify(msg, vim.log.levels.WARN, opts)
 end
 
 ---Log an error message
 ---@param msg string
-function M.notify_error(msg)
-  safe_notify(msg, vim.log.levels.ERROR)
+---@param opts? NotifyOpts
+function M.notify_error(msg, opts)
+  safe_notify(msg, vim.log.levels.ERROR, opts)
 end
 
 return M
