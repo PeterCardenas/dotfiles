eebe2acb0c7ea6c0c8f3376652b7e6adef411ef1
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-01-04 22:04:24 -0800

    🔧 agnostic way of getting ghostty pid

--- dot_config/fish/functions/reload_ghostty_config.fish
+++ dot_config/fish/functions/reload_ghostty_config.fish
@@ -1,10 +1,30 @@
 function reload_ghostty_config --description "Reload ghostty configuration by sending USR2 signal"
-    set -l ghostty_pid (ps -eo pid,comm | awk '$2 == "/Applications/Ghostty.app/Contents/MacOS/ghostty" {print $1}')
+    # Get all ghostty process PIDs
+    set -l ghostty_pids (ps -eo pid,comm | awk '$2 ~ /ghostty$/ {print $1}')
 
-    if test -z "$ghostty_pid"
+    if test -z "$ghostty_pids"
         return 1
     end
 
-    kill -USR2 $ghostty_pid
-    return $status
+    # Walk up the parent process chain to find which ghostty instance we're running in
+    set -l current_pid $fish_pid
+    while test $current_pid -gt 1
+        # Check if current_pid matches any ghostty PID
+        for gpid in $ghostty_pids
+            if test $current_pid -eq $gpid
+                # Found the ghostty instance that owns this shell
+                kill -USR2 $gpid
+                return $status
+            end
+        end
+
+        # Get parent PID
+        set current_pid (ps -o ppid= -p $current_pid 2>/dev/null | string trim)
+
+        # Break if we couldn't get parent (reached init or error)
+        if test -z "$current_pid"
+            return 1
+        end
+    end
+    return 1
 end
