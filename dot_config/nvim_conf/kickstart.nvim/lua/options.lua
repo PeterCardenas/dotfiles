4e4fa9a6f15e9ff6caabcbae3dc46e64c8530f3a
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-01-06 11:59:58 -0800

    🔧 remove numbers in foldcolumn

--- dot_config/nvim_conf/kickstart.nvim/lua/options.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/options.lua
@@ -9,7 +9,7 @@ vim.o.foldcolumn = '1'
 vim.o.foldlevel = 99
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
-vim.o.fillchars = 'eob: ,fold: ,foldopen:+,foldsep:│,foldclose:-'
+vim.o.fillchars = 'eob: ,fold: ,foldopen:+,foldsep:│,foldclose:-,foldinner: '
 
 vim.o.swapfile = false
 
