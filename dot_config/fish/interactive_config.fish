b14f0a22a26a0838f703bb04fb2880a63fa28489
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-01-07 00:18:33 -0800

    🔧 update ghostty nav after command and on shell start

--- dot_config/fish/interactive_config.fish
+++ dot_config/fish/interactive_config.fish
@@ -38,13 +38,15 @@ if test $status -ge 2
     ssh-agent -a $SSH_AUTH_SOCK &>/dev/null
 end
 
-function refresh_ghostty_nav --on-event fish_preexec
+function refresh_ghostty_nav --on-event fish_postexec
     if set -q TMUX
         ghostty_nvim_nav
     else
         ghostty_nvim_nav h,j,k,l
     end
 end
+
+refresh_ghostty_nav
 
 # Add ghostty completions
 set -l GHOSTTY_COMPLETION_PATH /Applications/Ghostty.app/Contents/Resources/fish/vendor_completions.d/ghostty.fish
