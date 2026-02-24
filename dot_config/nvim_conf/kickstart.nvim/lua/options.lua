5940c272548c7251db15654f48524323007162a1
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-02-23 16:29:01 -0800

    🔧 add sky files as bzl filetype

--- dot_config/nvim_conf/kickstart.nvim/lua/options.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/options.lua
@@ -267,6 +267,7 @@ vim.api.nvim_create_autocmd('FileType', {
 vim.filetype.add({
   extension = {
     mdx = 'markdown.mdx',
+    sky = 'bzl',
     swcrc = 'json',
     mdc = 'markdown',
     swiftinterface = 'swift',
