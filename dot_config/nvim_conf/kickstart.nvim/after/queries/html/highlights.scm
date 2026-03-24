4fff254d96c3a1654b10c247fefedd95a68a3f96
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-03-23 17:26:48 -0700

    🔧 ignore and hide cursor.com buttons

--- dot_config/nvim_conf/kickstart.nvim/after/queries/html/highlights.scm
+++ dot_config/nvim_conf/kickstart.nvim/after/queries/html/highlights.scm
@@ -3,3 +3,27 @@
 ; conceal comments for octo.nvim
 ((comment) @conceal
   (#maybe-conceal-whole-line! @conceal))
+
+; conceal cursor.com anchor tags
+((element
+  (start_tag
+    (tag_name) @_tag
+    (#eq? @_tag "a")
+    (attribute
+      (attribute_name) @_attr
+      (#eq? @_attr "href")
+      (quoted_attribute_value
+        (attribute_value) @_val
+        (#match? @_val "^https://cursor\\.com"))))) @conceal
+  (#set! conceal_lines ""))
+
+(element
+  (start_tag
+    (tag_name) @_tag
+    (#eq? @_tag "img")
+    (attribute
+      (attribute_name) @_attr_name
+      (#eq? @_attr_name "src")
+      (quoted_attribute_value
+        (attribute_value))))
+  (#set! image.ignore ""))
