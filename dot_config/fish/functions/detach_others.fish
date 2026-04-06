63886d890380eeb756ffd7f29d87c86d8e6d63ad
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-04-06 15:18:51 -0700

    🧹 log detached sessions

--- dot_config/fish/functions/detach_others.fish
+++ dot_config/fish/functions/detach_others.fish
@@ -6,9 +6,18 @@ function detach_others --description "Detach all other tmux clients from the cur
 
     set -l current_tty (tmux display-message -p '#{client_tty}')
 
+    set -l detached 0
+
     for client in (tmux list-clients -F '#{client_tty}')
         if test "$client" != "$current_tty"
             tmux detach-client -t "$client"
+            set detached (math $detached + 1)
         end
+    end
+
+    if test $detached -eq 0
+        print_info "No other clients to detach."
+    else
+        print_info "Detached $detached client(s)."
     end
 end
