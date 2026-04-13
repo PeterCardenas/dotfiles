9fcff5ed3001656789a2ddcf90a03f14f2627ae2
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-04-12 19:02:03 -0700

    🧹 print to stderr

--- dot_config/fish/functions/print_warn.fish
+++ dot_config/fish/functions/print_warn.fish
@@ -1,9 +1,9 @@
 function print_warn -a warn_msg
-    if status is-command-substitution; or not isatty stdout
+    if not isatty stderr
         return
     end
-    set_color yellow
-    echo -n "[WARN]"
-    set_color normal
-    echo ": $warn_msg"
+    set_color yellow >&2
+    echo -n "[WARN]" >&2
+    set_color normal >&2
+    echo ": $warn_msg" >&2
 end
