56df1f1cf30b8bebfe6568466491fc5e3e984922
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-04-12 19:02:13 -0700

    🧹 add error logs to reload_ghostty_config

--- dot_config/fish/functions/reload_ghostty_config.fish
+++ dot_config/fish/functions/reload_ghostty_config.fish
@@ -1,11 +1,13 @@
 function reload_ghostty_config --description "Reload ghostty configuration by sending USR2 signal"
     if not test "$TERM" = xterm-ghostty; or set -q TMUX; or set -q SSH_CONNECTION
+        print_error "This function is only intended to be used in ghostty sessions"
         return 1
     end
     # Get all ghostty process PIDs
     set -l ghostty_pids (ps -eo pid,comm | awk '$2 ~ /ghostty$/ {print $1}')
 
     if test -z "$ghostty_pids"
+        print_error "No ghostty processes found"
         return 1
     end
 
@@ -16,8 +18,13 @@ function reload_ghostty_config --description "Reload ghostty configuration by se
         for gpid in $ghostty_pids
             if test $current_pid -eq $gpid
                 # Found the ghostty instance that owns this shell
+                print_info "Reloading ghostty config (PID: $gpid)"
                 kill -USR2 $gpid
-                return $status
+                set -l kill_status $status
+                if test $kill_status -ne 0
+                    print_error "Failed to send USR2 signal to ghostty (PID: $gpid)"
+                end
+                return $kill_status
             end
         end
 
@@ -26,8 +33,10 @@ function reload_ghostty_config --description "Reload ghostty configuration by se
 
         # Break if we couldn't get parent (reached init or error)
         if test -z "$current_pid"
+            print_error "Could not find parent ghostty process"
             return 1
         end
     end
+    print_error "Could not find owning ghostty instance"
     return 1
 end
