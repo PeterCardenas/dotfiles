7ffdcb3f890322916d0e50d457da3e31ee2b6bc6
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-04-16 01:43:50 -0700

    feat(nvim.ts): highlight filepaths in treesitter language

--- dot_config/nvim_conf/kickstart.nvim/after/queries/markdown/injections.scm
+++ dot_config/nvim_conf/kickstart.nvim/after/queries/markdown/injections.scm
@@ -1,5 +1,11 @@
 ; extends
 
+(fenced_code_block
+  (info_string
+    (language) @_label
+    (#agentic-fence-path-inject! @_label))
+  (code_fence_content) @injection.content)
+
 ((inline) @injection.content
   (#lua-match? @injection.content "^%s*import")
   (#set! injection.language "typescript"))
