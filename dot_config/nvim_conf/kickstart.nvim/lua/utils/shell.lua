41529ee94afb921ea935d41a3c9d253f3f2d3df8
Peter Cardenas <16930781+PeterCardenas@users.noreply.github.com>
2026-04-22 05:41:53 -0700

    refactor(nvim.utils): add async.wrap helper

--- dot_config/nvim_conf/kickstart.nvim/lua/utils/shell.lua
+++ dot_config/nvim_conf/kickstart.nvim/lua/utils/shell.lua
@@ -1,3 +1,4 @@
+local Async = require('utils.async')
 local String = require('utils.string')
 local Log = require('utils.log')
 M = {}
@@ -16,101 +17,67 @@ end
 ---@field stdin? string
 ---@field detach? boolean
 
----@type (async fun(cmd: string, args: string[], opts: ShellAsyncOpts | nil): (boolean, string[])) | nil
-local cached_async_cmd = nil
+---@type async fun(cmd: string, args: string[], opts: ShellAsyncOpts | nil): (boolean, string[])
+M.async_cmd = Async.wrap(
+  ---@param cmd string
+  ---@param args string[]
+  ---@param opts ShellAsyncOpts | nil
+  ---@param done fun(success: boolean, output: string[])
+  ---@return nil
+  function(cmd, args, opts, done)
+    opts = opts or {}
+    local Job = require('plenary.job')
 
-local function get_async_cmd()
-  if cached_async_cmd == nil then
-    local async = require('plenary.async')
-    cached_async_cmd = async.wrap(
-      ---@param cmd string
-      ---@param args string[]
-      ---@param opts ShellAsyncOpts | nil
-      ---@param done fun(success: boolean, output: string[])
-      ---@return nil
-      function(cmd, args, opts, done)
-        opts = opts or {}
-        local Job = require('plenary.job')
-
-        ---@type string[]
-        local output = {}
-        ---@param data string
-        local function handle_output(data)
-          if data then
-            local lines = String.split_lines(data)
-            vim.list_extend(output, lines)
-          end
+    ---@type string[]
+    local output = {}
+    ---@param data string
+    local function handle_output(data)
+      if data then
+        local lines = String.split_lines(data)
+        vim.list_extend(output, lines)
+      end
+    end
+    ---@diagnostic disable-next-line: missing-fields
+    local job = Job:new({
+      command = cmd,
+      args = args,
+      writer = opts.stdin,
+      detached = opts.detach,
+      on_stdout = function(_, data)
+        handle_output(data)
+      end,
+      on_stderr = function(_, data)
+        handle_output(data)
+      end,
+      on_exit = function(_, code)
+        ---@type boolean, string
+        local ok, err = pcall(done, code == 0, output)
+        if not ok then
+          Log.notify_error(
+            'Error calling done callback:' .. err .. '\ncmd: ' .. cmd .. '\nargs: ' .. vim.inspect(args) .. '\nOutput: ' .. table.concat(output, '\n')
+          )
         end
-        ---@diagnostic disable-next-line: missing-fields
-        local job = Job:new({
-          command = cmd,
-          args = args,
-          writer = opts.stdin,
-          detached = opts.detach,
-          on_stdout = function(_, data)
-            handle_output(data)
-          end,
-          on_stderr = function(_, data)
-            handle_output(data)
-          end,
-          on_exit = function(_, code)
-            ---@type boolean, string
-            local ok, err = pcall(done, code == 0, output)
-            if not ok then
-              Log.notify_error(
-                'Error calling done callback:' .. err .. '\ncmd: ' .. cmd .. '\nargs: ' .. vim.inspect(args) .. '\nOutput: ' .. table.concat(output, '\n')
-              )
-            end
-          end,
-          env = vim.tbl_extend('force', vim.fn.environ(), {
-            GH_TOKEN = GH_TOKEN,
-          }),
-          cwd = opts.cwd,
-        })
-        job:start()
       end,
-      4
-    )
-  end
-  return cached_async_cmd
-end
-
----Run a shell command asynchronously and return the output.
----@async
----@param cmd string
----@param args string[]
----@param opts? ShellAsyncOpts
----@return boolean, string[]
-function M.async_cmd(cmd, args, opts)
-  local async_cmd = get_async_cmd()
-  return async_cmd(cmd, args, opts)
-end
-
----@type async fun(ms: number)
-local cached_sleep = nil
-
-local get_cached_sleep = function()
-  if cached_sleep == nil then
-    local async = require('plenary.async')
-    cached_sleep = async.wrap(
-      ---@param ms number
-      ---@param done fun(): nil
-      function(ms, done)
-        vim.defer_fn(function()
-          done()
-        end, ms)
-      end,
-      2
-    )
-  end
-  return cached_sleep
-end
+      env = vim.tbl_extend('force', vim.fn.environ(), {
+        GH_TOKEN = GH_TOKEN,
+      }),
+      cwd = opts.cwd,
+    })
+    job:start()
+  end,
+  4
+)
 
 ---Sleep asynchrnously for a given number of milliseconds.
----@async
----@param ms number
-function M.sleep(ms)
-  return get_cached_sleep()(ms)
-end
-
+---@type async fun(ms: number)
+M.sleep = Async.wrap(
+  ---@param ms number
+  ---@param done fun(): nil
+  function(ms, done)
+    vim.defer_fn(function()
+      done()
+    end, ms)
+  end,
+  2
+)
 return M
